3cfa5a6867b9a6f7a64c919e6b0927eb639bbd31
Alex Dunn <dunn.alex@gmail.com>
2018-07-18 09:50:47 -0700

    do batch imports right

--- geniza/lib/importer/exhibit.rb
+++ geniza/lib/importer/exhibit.rb
@@ -1,3 +1,4 @@
+
 # frozen_string_literal: true
 
 require 'erb'
@@ -18,11 +19,8 @@ module Importer::Exhibit
       table = ::CSV.table(m, encoding: 'UTF-8')
 
       exhibit_row = table.select do |row|
-        # byebug
         row[CONFIG[:exhibit_type_key].to_sym] == CONFIG[:exhibit_class]
       end.first
-
-      # byebug
 
       exhibit_params = {
         title: exhibit_row[CONFIG[:exhibit_title_key].to_sym],
@@ -30,40 +28,31 @@ module Importer::Exhibit
       }
 
       exhibit = Spotlight::Exhibit.create(exhibit_params)
-
       objects = table.reject { |row| row == exhibit_row }.map do |row|
-        file = Parse.get_binary_paths(Array(row[:files]), data).first
+        resource = Spotlight::Resources::Upload.new(
+          data: {
+            'full_title_tesim' => row[CONFIG[:object_title_key].to_sym],
+            'spotlight_upload_description_tesim' => row[CONFIG[:object_description_key].to_sym],
+            'spotlight_upload_attribution_tesim' => row[CONFIG[:object_attribution_key].to_sym],
+            'spotlight_upload_date_tesim' => row[CONFIG[:object_date_key].to_sym].to_s
+          },
+          exhibit: exhibit
+        )
 
-        {
-          attribution: row[CONFIG[:object_attribution_key].to_sym],
-          base64: Base64.encode64(File.read(file)).delete("\n"),
-          date: row[CONFIG[:object_date_key].to_sym],
-          description: row[CONFIG[:object_description_key].to_sym],
-          filename: file,
-          mime: MIME::Types.type_for(file).first.content_type,
-          title: row[CONFIG[:object_title_key].to_sym]
-        }
+        file = Parse.get_binary_paths(Array(row[:files]), data).first
+        logger.debug "Attaching #{file}"
+
+        image = Spotlight::FeaturedImage.new
+        image.image.store!(File.open(file))
+
+        resource.upload = image
+        resource.save_and_index
+        resource
       end
 
-      erb_vars = OpenStruct.new(
-        created: Time.zone.now.strftime('%FT%T.%LZ'),
-        exhibit: exhibit_params,
-        objects: objects
-      ).instance_eval { binding }
-
-      json = JSON.parse(
-        ERB.new(
-          File.read(Rails.root.join('lib', 'importer', 'data.json.erb'))
-        ).result(erb_vars)
-      )
-
-      # File.open(Rails.root.join('tmp', 'ex.json'), 'a') do |f|
-      #   f.write json
-      # end
-
-      exhibit.import(json)
+      exhibit.resources = objects
       exhibit.save
-      # exhibit.reindex_later
+      exhibit.reindex_later
     end
   end
 end
