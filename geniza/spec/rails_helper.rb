4710c6c0437fe91f9dc084eb9aa47db23d044ddb
Bess Sadler <bess@curationexperts.com>
2018-11-30 12:12:25 -0500

    Feature test for creating an exhibit via the UI
    Create a new exhibit
    Add an item via an uploaded CSV
    Ensure the item got added as expected
    
    Clean database and solr between test runs
    
    Use database-cleaner gem to truncate database
    between test runs
    Trigger a delete-all to solr between test runs
    
    This ensures that tests which write to solr have
    a clean environment and can run consistently.
    
    Travis should explicitly called rake ci
    Use headless capybara driver by default
    Travis needs a secrets file to run
    Chrome driver set up for travis
    Use standard spotlight
    Add db:test:prepare to fix travis build

--- geniza/spec/rails_helper.rb
+++ geniza/spec/rails_helper.rb
@@ -25,6 +25,32 @@ require 'database_cleaner'
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
+# Uses faster rack_test driver when JavaScript support not needed
+Capybara.default_driver = :rack_test
+
+Capybara.register_driver :chrome do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--disable-gpu'
+  browser_options.args << '--no-sandbox'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+Capybara.javascript_driver = :chrome
+
+Capybara.default_max_wait_time = 10
+
+
+# These settings spin up a local browser and let you watch the tests in the browser
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+#
+# Capybara.configure do |config|
+#   config.default_max_wait_time = 10 # seconds
+#   config.default_driver        = :selenium
+# end
+
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -36,11 +62,26 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.before :suite do
+    DatabaseCleaner.clean
+  end
+
+  config.before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+    Blacklight.default_index.connection.delete_by_query('*:*', params: { 'softCommit' => true })
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
