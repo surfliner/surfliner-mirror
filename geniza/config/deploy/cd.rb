161332f771dfeb1dc0bb077e4ca12ede468327f8
Bess Sadler <bess@curationexperts.com>
2018-12-04 11:35:09 -0500

    DCE deploys need the blacklight.yml file that's on the DCE servers (#15)
    We specify SOLR_URL differently from how UCSB does.

--- geniza/config/deploy/cd.rb
+++ geniza/config/deploy/cd.rb
@@ -3,6 +3,7 @@ server 'geniza-cd.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
 set :deploy_to, ENV.fetch('TARGET', '/opt/geniza')
 set :rails_env, 'production'
 append :linked_files, "config/database.yml"
+append :linked_files, "config/blacklight.yml"
 set :assets_prefix, "#{shared_path}/public/assets"
 append :linked_files, ".env.production"
 set :ssh_options, keys: ["geniza-cd-key"] if File.exist?("geniza-cd-key")
