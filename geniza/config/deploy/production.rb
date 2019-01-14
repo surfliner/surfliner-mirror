8baeede4245fe38483ded9f7a053ff8f4d6946d7
Alex Dunn <dunn.alex@gmail.com>
2019-01-14 11:44:52 -0800

    capistrano: fix deploy in light of yarn changes

--- geniza/config/deploy/production.rb
+++ geniza/config/deploy/production.rb
@@ -5,8 +5,8 @@ set :rails_env, 'production'
 
 # Uncomment if deploying to a server without a public IP
 # set :default_env,
-#     'http_proxy' => '',
-#     'https_proxy' => ''
+#     'http_proxy' => 'http://10.3.100.201:3128',
+#     'https_proxy' => 'http://10.3.100.201:3128'
 
 server ENV['SERVER'], user: 'adrl', roles: [:web, :app, :db]
 set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
