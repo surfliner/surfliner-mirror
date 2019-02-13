ccd44191a90815f890bc8494abb841261ea57a6a
Alex Dunn <dunn.alex@gmail.com>
2019-02-13 12:52:27 -0800

    [starlight] move render_type logic to ApplicationHelper

--- starlight/app/helpers/application_helper.rb
+++ starlight/app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
   def iiif_manifest(manifest_service:)
     manifest_service.url
   end
+
+  def resource_upload_path(document)
+    Spotlight::Resources::Upload.find(document.id.to_s.split('-').last).upload.image.to_s
+  end
+
+  def resource_render_type(document)
+    is_pdf = (File.extname(resource_upload_path(document)) == '.pdf')
+
+    (is_pdf ? "catalog/pdf" : "catalog/universal_viewer")
+  end
 end
