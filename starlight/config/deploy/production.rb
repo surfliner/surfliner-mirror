d8d9fca1504721c46e15a7971518d37fe38d6df7
Matt Critchlow <mcritchlow@ucsd.edu>
2019-03-11 08:46:49 -0700

    starlight: remove proxy comments in cap deploy

--- starlight/config/deploy/production.rb
+++ starlight/config/deploy/production.rb
@@ -3,11 +3,6 @@
 set :stage, :production
 set :rails_env, 'production'
 
-# Uncomment if deploying to a server without a public IP
-# set :default_env,
-#     'http_proxy' => 'http://10.3.100.201:3128',
-#     'https_proxy' => 'http://10.3.100.201:3128'
-
 server ENV['SERVER'], user: ENV['CAP_USER'], roles: [:web, :app, :db]
 set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
 
--- starlight/config/deploy/staging.rb
+++ starlight/config/deploy/staging.rb
@@ -3,10 +3,5 @@
 set :stage, :staging
 set :rails_env, 'staging'
 
-# Uncomment if deploying to a server without a public IP
-# set :default_env,
-#     'http_proxy' => 'http://10.3.100.201:3128',
-#     'https_proxy' => 'http://10.3.100.201:3128'
-
 server ENV['SERVER'], user: ENV['CAP_USER'], roles: [:web, :app, :db]
 set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
