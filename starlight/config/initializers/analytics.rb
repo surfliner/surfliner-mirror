5ec0bee0a510c74716b7692c3f1a0b02e602764a
Matt Critchlow <mcritchlow@ucsd.edu>
2019-03-14 14:39:52 -0700

    starlight: don't fail if GA isn't setup

--- starlight/config/initializers/analytics.rb
+++ starlight/config/initializers/analytics.rb
@@ -1,6 +1,6 @@
 # Setup Google Analytics in production environment
 if Rails.env.production?
-  Spotlight::Engine.config.ga_pkcs12_key_path = ENV.fetch('GA_PKCS12_KEY_PATH')
-  Spotlight::Engine.config.ga_web_property_id = ENV.fetch('GA_WEB_PROPERTY_ID')
-  Spotlight::Engine.config.ga_email = ENV.fetch('GA_EMAIL')
+  Spotlight::Engine.config.ga_pkcs12_key_path = ENV['GA_PKCS12_KEY_PATH']
+  Spotlight::Engine.config.ga_web_property_id = ENV['GA_WEB_PROPERTY_ID']
+  Spotlight::Engine.config.ga_email = ENV['GA_EMAIL']
 end
