e1591948018d57113dcf7f86f09f8a431fccfdbe
Matt Critchlow <mcritchlow@ucsd.edu>
2019-03-21 09:54:38 -0700

    starlight: remove external file dep in system test
    - Remove the dependency on the dog image in create_exhibit_spec
    - Add a local fixture image from the UCSD DAMS
    https://library.ucsd.edu/dc/object/bb8772606z
    - Update url_single_item_exhibit.csv to file_single_item_exhibit.csv to
    reflect that it's now pulling from a local file

--- starlight/spec/system/create_exhibit_spec.rb
+++ starlight/spec/system/create_exhibit_spec.rb
@@ -4,10 +4,11 @@ require "rails_helper"
 
 RSpec.describe "Import and Display a Work", :clean, type: :system, js: true do
   let(:csv_file_path)   { File.join(fixture_path, csv_file_name) }
-  let(:csv_file_name)   { "url_single_item_exhibit.csv" }
+  let(:csv_file_name)   { "file_single_item_exhibit.csv" }
   let(:site_admin)      { FactoryBot.create(:omniauth_site_admin) }
 
   before do
+    ENV["BINARY_ROOT"] = Rails.root.join("spec", "fixtures", "images").to_s
     omniauth_setup_shibboleth_for(site_admin)
     sign_in
     allow(Spotlight::DefaultThumbnailJob).to receive(:perform_later)
@@ -35,13 +36,13 @@ RSpec.describe "Import and Display a Work", :clean, type: :system, js: true do
       page.attach_file("resources_csv_upload[url]", csv_file_path)
       click_button "Add item"
       visit "/spotlight/test-exhibit/catalog?utf8=%E2%9C%93&exhibit_id=test-exhibit&search_field=all_fields&q="
-      expect(page).to have_content "A Cute Dog"
+      expect(page).to have_content "Colima dog in Santa Rosilia"
 
       # Test that the exhibit page is accessible to non-admin users
       sign_out
       visit "/spotlight/test-exhibit/catalog?utf8=%E2%9C%93&exhibit_id=test-exhibit&search_field=all_fields&q="
-      expect(page).to have_content "A Cute Dog"
-      click_link "A Cute Dog"
+      expect(page).to have_content "Colima dog in Santa Rosilia"
+      click_link "Colima dog in Santa Rosilia"
       # IIIF Manifest present on the page
       expect(page.html).to match(%r{/spotlight/.*/manifest})
       expect(page).to have_selector ".universal-viewer-iframe"
