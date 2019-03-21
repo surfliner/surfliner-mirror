e77de1e9d0495e187db8223bf343e211341ce896
Matt Critchlow <mcritchlow@ucsd.edu>
2019-03-21 08:51:20 -0700

    starlight: limit resource usage for selenium

--- starlight/spec/support/capybara_selenium.rb
+++ starlight/spec/support/capybara_selenium.rb
@@ -23,11 +23,17 @@ RSpec.configure do |config|
       # make test app listen to outside requests (selenium container)
       Capybara.server_host = "0.0.0.0"
 
+      capabilities = {
+        chromeOptions: {
+          args: %w[headless disable-gpu disable-dev-shm-usage], # preserve memory & cpu consumption
+        },
+      }
+
       driven_by :selenium,
                 using: :chrome,
                 options: { browser: :remote,
                            url: ENV["SELENIUM_URL"],
-                           desired_capabilities: :chrome, }
+                           desired_capabilities: capabilities, }
     else
       driven_by :selenium_chrome_headless
     end
