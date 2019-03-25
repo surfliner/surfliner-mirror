27173960094e3ededd2f6df78b1fe6fb5bf7d3c2
Alex Dunn <dunn.alex@gmail.com>
2019-03-25 10:29:49 -0700

    starlight: fix deploy for UCSB

--- starlight/config/deploy/production.rb
+++ starlight/config/deploy/production.rb
@@ -3,7 +3,11 @@
 set :stage, :production
 set :rails_env, "production"
 
-server ENV.fetch("SERVER"), user: ENV.fetch("CAP_USER"), roles: [:web, :app, :db]
-set :ssh_options, port: 22, keys: [ENV.fetch("KEYFILE")]
+set :default_env,
+    "http_proxy" => ENV["CAP_PROXY"],
+    "https_proxy" => ENV["CAP_PROXY"]
 
-after "deploy:restart", "sitemap:refresh" if ENV["SITEMAP_REFRESH"].present?
+server ENV.fetch("SERVER"), user: ENV.fetch("CAP_USER"), roles: [:web, :app, :db]
+set :ssh_options, port: 22, keys: [ENV.fetch("KEYFILE") { "~/.ssh/id_rsa" }]
+
+after "deploy:restart", "sitemap:refresh" if ENV["SITEMAP_REFRESH"]
