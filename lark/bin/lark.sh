b6e31886cec671c484982a2ec789daaf322425ce
Tom Johnson <tomjohnson@ucsb.edu>
2019-04-11 12:50:33 -0700

    lark: Move database setup to a `lark.sh` startup script
    Moves database migrations from the `docker-compose` configuration to a startup
    script. This avoids isolating the database setup to the compose environment.

---
+++ lark/bin/lark.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT;
+do
+  echo "waiting for postgresql";
+  sleep 1;
+done;
+
+bundle exec rake db:migrate &&
+  bundle exec rackup --host 0.0.0.0 -p 5000
