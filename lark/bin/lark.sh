3ea0902064d0aa9aa99291a98945a821a865fbe3
Tom Johnson <tomjohnson@ucsb.edu>
2019-04-11 16:26:53 -0700

    lark: Add `EVENT_ADAPTER` to helm questions, use `memory` by default
    Fixes Lark depolyment by adding configuration for the event adapter.
    
    Follow up work will introduce Postgresql and configure it as the default.

--- lark/bin/lark.sh
+++ lark/bin/lark.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT;
-do
-  echo "waiting for postgresql";
-  sleep 1;
-done;
+if [ $EVENT_ADAPTER == "sql" ]
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT;
+  do
+    echo "waiting for postgresql";
+    sleep 1;
+  done;
+fi
 
 bundle exec rake db:migrate &&
   bundle exec rackup --host 0.0.0.0 -p 5000
