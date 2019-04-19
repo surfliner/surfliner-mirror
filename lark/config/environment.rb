eaf4ece3a72965eb2372cbcf0683578b3757f113
Tom Johnson <tomjohnson@ucsb.edu>
2019-04-19 13:28:17 -0700

    lark: Only register the Lark :sql and :solr adapters when used
    Registering the adapters attempts to create a connection, this makes it
    impossible to deploy the index adapter to an environment that doesn't have a
    properly configured postgres connection.
    
    Presently, that includes `lark-dev` and `lark-staging`.

--- lark/config/environment.rb
+++ lark/config/environment.rb
@@ -8,39 +8,44 @@ require 'bundler'
 
 Bundler.require
 
+Lark.config.event_adapter = ENV['EVENT_ADAPTER'].to_sym
+Lark.config.index_adapter = ENV['INDEX_ADAPTER'].to_sym
+
 Valkyrie::MetadataAdapter.register(
   Valkyrie::Persistence::Memory::MetadataAdapter.new,
   :memory
 )
 
-##
-# Register postgres metadata adapter
-Valkyrie::MetadataAdapter.register(
-  Valkyrie::Sequel::MetadataAdapter.new(
-    connection: Sequel.connect(DATABASE)
-  ),
-  :sql
-)
+if Lark.config.event_adapter == :sql
+  ##
+  # Register postgres metadata adapter
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Sequel::MetadataAdapter.new(
+      connection: Sequel.connect(DATABASE)
+    ),
+    :sql
+  )
+end
 
-##
-# Register solr metadata adapter with RSolr connection for SOLR persistence
-Valkyrie::MetadataAdapter.register(
-  Valkyrie::Persistence::Solr::MetadataAdapter
-    .new(connection: RSolr.connect(url: ENV['SOLR_URL'])),
-  :solr
-)
+if Lark.config.index_adapter == :solr
+  ##
+  # Register solr metadata adapter with RSolr connection for SOLR persistence
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Persistence::Solr::MetadataAdapter
+      .new(connection: RSolr.connect(url: ENV['SOLR_URL'])),
+    :solr
+  )
 
-# Register custom queries for the default Valkyrie metadata adapter
-# (see Valkyrie::Persistence::CustomQueryContainer)
-Valkyrie::MetadataAdapter.find(Lark.config.index_adapter)
-                         .query_service
-                         .custom_queries
-                         .register_query_handler(FindByStringProperty)
+  # Register custom queries for the default Valkyrie metadata adapter
+  # (see Valkyrie::Persistence::CustomQueryContainer)
+  Valkyrie::MetadataAdapter
+    .find(Lark.config.index_adapter)
+    .query_service
+    .custom_queries
+    .register_query_handler(FindByStringProperty)
+end
 
 require_relative 'initializers/healthchecks'
 require_relative 'initializers/healthchecks_complete'
-
-Lark.config.event_adapter = ENV['EVENT_ADAPTER'].to_sym
-Lark.config.index_adapter = ENV['INDEX_ADAPTER'].to_sym
 
 Lark.config.event_stream.subscribe(IndexListener.new)
