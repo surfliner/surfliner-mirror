f72f0a5fb5b5c4c51f01bda8e69d5192bda253f1
lsitu <lsitu@ucsd.edu>
2019-04-24 12:40:33 -0700

    Refactor scripts and update display labels.

---
+++ lark-frontend/src/assets/scripts/lark.js
@@ -0,0 +1,101 @@
+const status = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response)
+  }
+  return Promise.reject(new Error(response.statusText))
+}
+
+const lark_url = 'http://localhost:9292/'
+const json = response => response.json()
+
+export default {
+  name: 'Lark',
+  data: function() {
+    return {
+      searchText: '',
+      page: '',
+      authorities: [],
+      item: {}
+    }
+  },
+  methods: {
+    async getAuthorities() {
+      this.authorities = [];
+      fetch(lark_url + 'search?pref_label=' + this.searchText, { mode: 'cors' })
+        .then(status)
+        .then(json)
+        .then(data => this.authorities = data)
+        .then(this.page = 'search')
+        .catch(error => { console.log('Request failed', error) });
+    },
+    async displayItem(id) {
+      fetch(lark_url + id, { mode: "cors" })
+        .then(status)
+        .then(json)
+        .then(data => this.item = data)
+        .then(this.page = 'item')
+        .catch(error => { console.log('Request failed', error) });
+    },
+    newAuthority() {
+      this.page = 'new';
+      this.item = {
+        "pref_label": [],
+        "alternate_label": [],
+        "hidden_label": [],
+        "exact_match": [],
+        "close_match": [],
+        "note": [],
+        "scope_note": [],
+        "editorial_note": [],
+        "history_note": [],
+        "definition": [],
+        "identifier": []
+      }
+    },
+    async createAuthority() {
+      this.axios.post(lark_url, this.item, { mode: 'cors', headers: { 'Content-Type': 'application/json' } })
+        .then(status)
+        .then(response => this.item = response.data)
+        .then(this.page = 'item')
+        .catch(error => { console.log('Request failed', error) });
+      
+    },
+    editAuthority() {
+      this.page = 'edit';
+      this.pref_label = this.item.pref_label[0];
+      this.alternate_label = this.item.alternate_label[0];
+    },
+    async updateAuthority() {
+      this.axios.put(lark_url + this.item.id, this.item, { mode: 'cors', headers: { 'Content-Type': 'application/json' } })
+        .then(status)
+        .then(this.page = 'item')
+        .catch(error => { console.log('Request failed', error) });
+    },
+    updateField(fieldName, e) {
+      this.$set(this.item, fieldName, e.target.value);
+      this.$emit('input', e.target.value)
+    },
+    fieldValue(value)
+    {
+      return value instanceof Array ? value[0] : value;
+    },
+    nomalizeLabel(value) 
+    {
+      var v = value.replace('_', ' ')
+      return v.charAt(0).toUpperCase() + v.slice(1);
+    }
+  },
+  computed: {
+    authoritiesCount() {
+      return this.authorities ? this.authorities.length : 0;
+    },
+    displayPage() {
+      return this.page;
+    },
+    displayAttributes() {
+      var dup = JSON.parse(JSON.stringify(this.item));
+      delete dup['id']
+      return dup;
+    }
+  }
+}
--- lark-frontend/src/components/Lark.vue
+++ lark-frontend/src/components/Lark.vue
@@ -17,13 +17,13 @@
             <tr v-for="(value, key) of item" :key="key">
               <td>
                 <label :for="key">{{ nomalizeLabel(key) }}</label>
-                <input type="text" class="field" :name="key" :value="value" @input="updateField(key, $event)" :placeholder="key">
+                <input type="text" class="field" :name="key" :value="value" @input="updateField(key, $event)">
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div><button type="button" v-on:click="createAuthority()" id="btnCreate">Submit</button></div>
+      <div><button type="button" v-on:click="createAuthority()" id="btnCreate">Create</button></div>
     </div>
 
     <div v-if="displayPage === 'edit'">
@@ -34,27 +34,27 @@
             <tr v-for="(value, key) of item" :key="key">
               <td v-if="key != 'id'">
                 <label :for="key">{{ nomalizeLabel(key) }}</label>
-                <input type="text" class="field" :name="key" :value="value" @input="updateField(key, $event)" :placeholder="key">
+                <input type="text" class="field" :name="key" :value="value" @input="updateField(key, $event)">
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-      <div><button type="button" v-on:click="updateAuthority()" id="btnUpdate">Submit</button></div>
+      <div><button type="button" v-on:click="updateAuthority()" id="btnUpdate">Update</button></div>
     </div>
 
     <div v-if="displayPage === 'item' && item.id">
       <div align="center">
         <table>
           <caption>
-            <span>Authority {{ fieldValue(item.pref_label) }}</span>
+            <span>{{ fieldValue(item.pref_label) }}</span>
             <button v-on:click="editAuthority()" id="btnEdit">Edit</button>
           </caption>
           <thead>
             <tr><th class="attribute" nowrap>Attribute Name</th><th>Values</th></tr>
           </thead>
           <tbody>
-          <tr v-for="(value, key) of item" :key="key" v-if="key != 'id'">
+          <tr v-for="(value, key) of displayAttributes" :key="key">
             <td>{{ nomalizeLabel(key) }}</td><td>{{ fieldValue(value) }}</td>
           </tr>
           </tbody>
@@ -79,104 +79,7 @@
   </div>
 </template>
 
-<script>
-const status = response => {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response)
-  }
-  return Promise.reject(new Error(response.statusText))
-}
-
-const lark_url = 'http://localhost:9292/'
-const json = response => response.json()
-
-export default {
-  name: 'Lark',
-  data: function() {
-    return {
-      searchText: '',
-      page: '',
-      authorities: [],
-      item: {}
-    }
-  },
-  methods: {
-    async getAuthorities() {
-      this.authorities = [];
-      fetch(lark_url + 'search?pref_label=' + this.searchText, { mode: 'cors' })
-        .then(status)
-        .then(json)
-        .then(data => this.authorities = data)
-        .then(this.page = 'search')
-        .catch(error => { console.log('Request failed', error) });
-    },
-    async displayItem(id) {
-      fetch(lark_url + id, { mode: "cors" })
-        .then(status)
-        .then(json)
-        .then(data => this.item = data)
-        .then(this.page = 'item')
-        .catch(error => { console.log('Request failed', error) });
-    },
-    newAuthority() {
-      this.page = 'new';
-      this.item = {
-        "pref_label": [],
-        "alternate_label": [],
-        "hidden_label": [],
-        "exact_match": [],
-        "close_match": [],
-        "note": [],
-        "scope_note": [],
-        "editorial_note": [],
-        "history_note": [],
-        "definition": [],
-        "identifier": []
-      }
-    },
-    async createAuthority() {
-      this.axios.post(lark_url, this.item, { mode: 'cors', headers: { 'Content-Type': 'application/json' } })
-        .then(status)
-        .then(response => this.item = response.data)
-        .then(this.page = 'item')
-        .catch(error => { console.log('Request failed', error) });
-      
-    },
-    editAuthority() {
-      this.page = 'edit';
-      this.pref_label = this.item.pref_label[0];
-      this.alternate_label = this.item.alternate_label[0];
-    },
-    async updateAuthority() {
-      this.axios.put(lark_url + this.item.id, this.item, { mode: 'cors', headers: { 'Content-Type': 'application/json' } })
-        .then(status)
-        .then(this.page = 'item')
-        .catch(error => { console.log('Request failed', error) });
-    },
-    updateField(fieldName, e) {
-      this.$set(this.item, fieldName, e.target.value);
-      this.$emit('input', e.target.value)
-    },
-    fieldValue(value)
-    {
-      return value instanceof Array ? value[0] : value;
-    },
-    nomalizeLabel(value) 
-    {
-      var v = value.replace('_', ' ')
-      return v.charAt(0).toUpperCase() + v.slice(1);
-    }
-  },
-  computed: {
-    authoritiesCount () {
-      return this.authorities ? this.authorities.length : 0;
-    },
-    displayPage () {
-      return this.page;
-    }
-  }
-}
-</script>
+<script src="../assets/scripts/lark.js"/>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
