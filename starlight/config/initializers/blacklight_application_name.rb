dba4593ce73e94cecdfb20c90f673d6fca8eb98d
Matt Critchlow <mcritchlow@ucsd.edu>
2019-05-14 14:56:53 -0700

    starlight: new application_name override strategy
    The current module_eval method replacement reliably failed the docker
    test suite. This strategy aliases the original method so it is available
    for calling as a backup

--- starlight/config/initializers/blacklight_application_name.rb
+++ starlight/config/initializers/blacklight_application_name.rb
@@ -2,7 +2,8 @@
 
 # Teach the Blacklight helper to pay attention to the Spotlight site names
 Blacklight::BlacklightHelperBehavior.module_eval do
+  alias_method :blacklight_application_name, :application_name
   def application_name
-    current_site.title || super
+    current_site.title || blacklight_application_name
   end
 end
