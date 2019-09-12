96ea83a3cee06352e999b656b7d5041d7adec8d3
Alex Dunn <dunn.alex@gmail.com>
2019-09-12 16:57:22 -0700

    helm: fix DB detection and setup

--- starlight/docker/docker-entrypoint.sh
+++ starlight/docker/docker-entrypoint.sh
@@ -4,14 +4,23 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /home/starlight/app/tmp/pids/server.pid
 
-# Extract DB host and port
-if [ -z ${DATABASE_URL+x} ]; then
-  echo "DATABASE_URL is not supplied, skipping database setup"
+# Get DB host and port
+if [ -z "${DATABASE_URL}" ]; then
+  db_host=${POSTGRES_HOST}
+  db_port=${POSTGRES_PORT}
 else
   db_host=$(echo "$DATABASE_URL" | cut -d "@" -f2 | cut -d "/" -f1 | cut -d ":" -f1)
   db_port=$(echo "$DATABASE_URL" | cut -d "@" -f2 | cut -d "/" -f1 | cut -d ":" -f2)
   # if port wasn't specified, use default PG port
   if [ "$db_port" = "$db_host" ]; then
+    db_port=5432
+  fi
+fi
+
+if [ -z "${db_host}" ]; then
+  echo "No database host information found; skipping db setup"
+else
+  if [ -z "${db_port}" ]; then
     db_port=5432
   fi
 
@@ -21,7 +30,7 @@ else
     sleep 1
   done
 
-  if [ -z ${DATABASE_COMMAND+x} ]; then
+  if [ -z "${DATABASE_COMMAND}" ]; then
     echo "DATABASE_COMMAND is not supplied, skipping database rake tasks"
   else
     # Support env var option for database command(s)
