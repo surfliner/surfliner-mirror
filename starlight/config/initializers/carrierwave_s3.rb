f3e23b10254c8ab9696b38da066a69949e768c4d
Alex Dunn <dunn.alex@gmail.com>
2019-10-29 15:16:27 -0700

    initializers/carrierwave_s3: use new helper methods

--- starlight/config/initializers/carrierwave_s3.rb
+++ starlight/config/initializers/carrierwave_s3.rb
@@ -3,38 +3,26 @@
 require "spotlight"
 
 Rails.application.config.after_initialize do
-  Spotlight::IiifManifestPresenter.class_eval do
-    def iiif_url
-      "#{ENV['IIIF_ENDPOINT']}/#{uploaded_resource.upload.id}%2f#{uploaded_resource.upload['image']}"
-    end
-  end
-
   # Get the Solr document built properly
   Spotlight::UploadSolrDocumentBuilder.class_eval do
     def add_image_dimensions(solr_hash)
-      uri = URI("#{ENV['IIIF_ENDPOINT']}/#{resource.upload_id}%2f#{resource.upload['image']}/info.json")
-      Net::HTTP.start(uri.host, uri.port, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+      uri = URI(IIIFHelpers.info_url(resource.upload_id, {}))
+
+      Net::HTTP.start(uri.host,
+                      uri.port,
+                      use_ssl: Rails.env.production?,
+                      verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
         request = Net::HTTP::Get.new uri
         response = http.request(request)
         image_info = JSON.parse(response.body) # Net::HTTPResponse object
-        solr_hash[:spotlight_full_image_width_ssm] = image_info['width']
-        solr_hash[:spotlight_full_image_height_ssm] = image_info['height']
+        solr_hash[:spotlight_full_image_width_ssm] = image_info["width"]
+        solr_hash[:spotlight_full_image_height_ssm] = image_info["height"]
       end
     end
 
     def add_file_versions(solr_hash)
-      url = "#{ENV['IIIF_ENDPOINT']}/#{resource.upload_id}%2f#{resource.upload['image']}/full/!400,400/0/default.jpg"
+      url = IIIFHelpers.image_path(resource.upload_id, size: "!400,400")
       solr_hash[Spotlight::Engine.config.thumbnail_field] = url
-    end
-  end
-
-  # Set the correct iiif_tilesource so FeaturedImage#iiif_url works
-  Spotlight::FeaturedImage.class_eval do
-    def set_tilesource_from_uploaded_resource
-      return if iiif_tilesource
-
-      self.iiif_tilesource = "#{ENV['IIIF_ENDPOINT']}/#{id}%2f#{image.model['image']}/info.json"
-      save
     end
   end
 end
@@ -52,16 +40,16 @@ module Spotlight
 end
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+  config.fog_provider = "fog/aws"
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV.fetch('S3_ACCESS_KEY_ID'),
-    aws_secret_access_key: ENV.fetch('S3_SECRET_ACCESS_KEY'),
-    host:                  ENV.fetch('S3_HOST'),
-    endpoint:              ENV.fetch('S3_ENDPOINT'),
-    path_style: true
+    provider: "AWS",
+    aws_access_key_id: ENV.fetch("S3_ACCESS_KEY_ID"),
+    aws_secret_access_key: ENV.fetch("S3_SECRET_ACCESS_KEY"),
+    host: ENV.fetch("S3_HOST"),
+    endpoint: ENV.fetch("S3_ENDPOINT"),
+    path_style: true,
   }
-  config.fog_directory  = ENV.fetch('S3_BUCKET')
+  config.fog_directory = ENV.fetch("S3_BUCKET")
   # config.fog_public     = false # optional, defaults to true
   # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   config.storage = :fog
---
+++ starlight/config/initializers/iiif_helpers.rb
@@ -0,0 +1,22 @@
+module IIIFHelpers
+  IIIF_BASE_URL = ENV["IIIF_BASE_URL"] || "http://localhost:8182/iiif/2"
+
+  def self.info_url(id, _options)
+    "#{IIIF_BASE_URL}/#{id}/info.json"
+  end
+
+  def self.info_path(id)
+    "#{IIIF_BASE_URL}/#{id}/info.json"
+  end
+
+  def self.image_path(id,
+                      region: "full",
+                      size:,
+                      rotation: "0",
+                      quality: "default",
+                      format: "jpg")
+    "#{IIIF_BASE_URL}/#{id}/#{region}/#{size}/#{rotation}/#{quality}.#{format}"
+  end
+end
+
+Spotlight::Engine.config.iiif_url_helpers = IIIFHelpers
