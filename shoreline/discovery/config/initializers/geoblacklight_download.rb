e824f71ea4338caaae2b992e8f8a8b5b13a8ab12
Vivian Chu <tchu@ucsd.edu>
2020-03-12 11:31:52 -0700

    shoreline: Fix rubocop offenses

--- shoreline/discovery/config/initializers/geoblacklight_download.rb
+++ shoreline/discovery/config/initializers/geoblacklight_download.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Geoblacklight
+  # Geoblacklight Download class
   class Download
     def self.file_path
       ENV['GEOBLACKLIGHT_DOWNLOAD_PATH'] || "#{Rails.root}/tmp/cache/downloads"
