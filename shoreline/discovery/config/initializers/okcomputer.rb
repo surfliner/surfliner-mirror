0a1e22bbf21a16b37dd4e6ce1b9bf861ec714f59
Alex Dunn <dunn.alex@gmail.com>
2020-06-09 11:58:54 -0700

    shoreline: add okcomputer checks for solr and geoserver
    Addresses #502.

--- shoreline/discovery/config/initializers/okcomputer.rb
+++ shoreline/discovery/config/initializers/okcomputer.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
-OkComputer.logger   = Rails.logger
+# Mount the health endpoint at /healthz
+# Review all health checks at /health/all
 OkComputer.mount_at = 'healthz'
+
+# Setup additional services
+
+solr_url = "http://#{ENV.fetch('SOLR_HOST')}:#{ENV.fetch('SOLR_PORT')}/solr/#{ENV.fetch('SOLR_CORE_NAME')}"
+OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(solr_url)
+
+class GeoServerCheck < OkComputer::Check
+  def check
+    conn = Geoserver::Publish::Connection.new(
+      'url' => "http://#{ENV.fetch('GEOSERVER_HOST')}:#{ENV.fetch('GEOSERVER_PORT')}/geoserver/rest",
+      'user' => ENV.fetch('GEOSERVER_USER'),
+      'password' => ENV.fetch('GEOSERVER_PASSWORD')
+    )
+
+    if conn.get(path: '/')
+      mark_message 'GeoServer is running'
+    else
+      mark_failure
+      mark_message 'Failed to connect to GeoServer'
+    end
+  end
+end
+
+OkComputer::Registry.register 'geoserver', GeoServerCheck.new
