07497b960c033707367d4476a3019ebd3335992a
Matt Critchlow <mcritchlow@ucsd.edu>
2020-06-22 09:50:39 -0700

    starlight: use healthz endpoint
    We're aligning on healthz across surfliner products. So:
    
    - Setup OKComputer mount as `healthz`
    - Update starlight livenessProbe and readinessProbe to look at `healthz`
      route

--- starlight/config/initializers/okcomputer.rb
+++ starlight/config/initializers/okcomputer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Mount the health endpoint at /health
-# Review all health checks at /health/all
-OkComputer.mount_at = "health"
+# Mount the health endpoint at /healthz
+# Review all health checks at /healthz/all
+OkComputer.mount_at = "healthz"
 
 # Setup additional services
 # OKComputer provides app/default and ActiveRecord 'out of the box'
