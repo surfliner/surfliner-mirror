63d95e986ed63024af69d153a89125b74a44e643
Alexandra Dunn <dunn.alex@gmail.com>
2020-07-02 13:30:28 -0700

    lark: consolidate docker-compose

--- lark/bin/upload-solr-configs.sh
+++ lark/bin/upload-solr-configs.sh
@@ -3,8 +3,9 @@
 COUNTER=0;
 
 while [ $COUNTER -lt 30 ]; do
+  echo "-- Looking for ZK (${ZK_HOST}:${ZK_PORT})..."
   if echo srvr | nc "${ZK_HOST}" "${ZK_PORT}" | grep -q "Mode"; then
-    echo "Uploading solrconfig.xml and schema.xml to ZooKeeper ..."
+    echo "-- Uploading solrconfig.xml and schema.xml to ZooKeeper ..."
     bundle exec rake lark:solrconfig['solr/config']
     exit 0
   fi
@@ -12,5 +13,5 @@ while [ $COUNTER -lt 30 ]; do
   sleep 5s
 done
 
-echo "Failed to connect to ZooKeeper after 5 minutes";
+echo "--- ERROR: failed to connect to ZooKeeper after 5 minutes";
 exit 1
