aba515e2d6c2348308d8c50956766ef94fab956d
Alexandra Dunn <dunn.alex@gmail.com>
2020-07-02 10:21:51 -0700

    shoreline: better logging on solr scripts

--- shoreline/discovery/bin/create-solr-collection.sh
+++ shoreline/discovery/bin/create-solr-collection.sh
@@ -2,18 +2,19 @@
 
 COUNTER=0;
 
-while [ $COUNTER -lt 120 ]; do
-  if nc "${SOLR_HOST}" "${SOLR_PORT}"; then
+while [ $COUNTER -lt 30 ]; do
+  echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
+  if nc -z "${SOLR_HOST}" "${SOLR_PORT}"; then
     # shellcheck disable=SC2143
     if [ ! "$(curl "${SOLR_HOST}:${SOLR_PORT}/api/collections?action=COLSTATUS" | grep -q "${SOLR_CORE_HAME}")" ]; then
-      echo "Collection ${SOLR_CORE_NAME} does not exist; creating ..."
+      echo "-- Collection ${SOLR_CORE_NAME} does not exist; creating ..."
       curl -H 'Content-type: application/json' -d "{create: {name: ${SOLR_CORE_NAME}, config: solrconfig, numShards: 1}}" "${SOLR_HOST}:${SOLR_PORT}/api/collections/"
     fi
     exit 0
   fi
   COUNTER=$(( COUNTER+1 ));
-  sleep 2s
+  sleep 5s
 done
 
-echo "Failed to create Solr core after 240 secs";
+echo "--- ERROR: failed to create Solr core after 5 minutes";
 exit 1
