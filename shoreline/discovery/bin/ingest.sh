11e1b1b70c9a4e5a7af92a32afacfc5833c5b326
Alexandra Dunn <dunn.alex@gmail.com>
2020-07-08 11:16:12 -0700

    shoreline: extract batch ingest script from chart
    Part of #535

---
+++ shoreline/discovery/bin/ingest.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+FILES=${*}
+
+COUNTER=0
+while [ $COUNTER -lt 120 ]; do
+  echo "-- Looking for GeoServer at ${GEOSERVER_HOST}:${GEOSERVER_PORT} ..."
+  if nc -z "${GEOSERVER_HOST}" "${GEOSERVER_PORT}" ; then
+    echo "-- Found GeoServer; ingesting..."
+
+    for shape in ${FILES}; do
+      echo "-- Ingesting ${shape}..."
+      bundle exec rake shoreline:publish["${shape}"]
+    done
+
+    exit 0
+  fi
+  COUNTER=$(( COUNTER+1 ));
+  sleep 2s
+done;
+echo "--- ERROR: Failed to find GeoServer after 240 secs"
+exit 1
