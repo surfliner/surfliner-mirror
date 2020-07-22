31fc4baa2d778c51ecf93da38cb2ed41bcb2ef4b
Matt Critchlow <mcritchlow@ucsd.edu>
2020-07-22 11:25:26 -0700

    starlight: setup ssl/cors rails config for starlight
    For SSL starlight deployments, we would like to apply the Rails SSL
    config settings as outlined in:
    
    https://api.rubyonrails.org/classes/ActionDispatch/SSL.html
    
    We're also setting the protect_from_forgery flag as this was observed to
    be an issue with other Spotlight exhibits https://github.com/projectblacklight/spotlight/issues/2085

--- starlight/config/application.rb
+++ starlight/config/application.rb
@@ -22,6 +22,9 @@ module Starlight
         [], [], true
       )[Rails.env] || {}
 
+    # Protect from forgery by default
+    config.action_controller.protect_from_forgery
+
     config.action_mailer.default_options = { from: ENV.fetch("FROM_EMAIL") }
   end
 end
