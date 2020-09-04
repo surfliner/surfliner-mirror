abdd00f674ff485036b29d756753a0e7bf242de6
tamsin johnson <tomjohnson@ucsb.edu>
2020-09-04 14:04:37 -0700

    starlight: make sidekiq containers wait for pending migrations
    in the past, the sidekiq deployment has been configured to wait for the webapp
    deployment to be ready. the reasoning for this is that the webapp deployment
    runs the database migrations, which are needed to start sidekiq.
    
    this refactor makes this wait explict. instead of waiting for the web service,
    this now checks for db availability and migration status directly. this should
    be more resilent as db setup becomes decoupled from the web service.

--- starlight/bin/db-wait.sh
+++ starlight/bin/db-wait.sh
@@ -3,8 +3,12 @@
 host=$(printf "%s\n" "$1"| cut -d : -f 1)
 port=$(printf "%s\n" "$1"| cut -d : -f 2)
 
+shift 1
+
 while ! nc -z "$host" "$port"
 do
   echo "waiting for $host:$port"
   sleep 1
 done
+
+exec "$@"
