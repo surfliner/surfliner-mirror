785e184002784727c347e932c883d5b8a93c3e37
tamsin johnson <tomjohnson@ucsb.edu>
2020-09-04 13:32:56 -0700

    starlight: do database migration as an init container
    move the starlight database migration out of the entrypoint and into an init
    container.
    
    this is more portable, and more resilient to possible future HA configurations.
    
    another option would be to move this setup to a `Job` that triggers for helm
    releases.

---
+++ starlight/bin/db-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+# Get DB host and port
+if [ -z "${DATABASE_URL}" ]; then
+  db_host=${POSTGRES_HOST}
+  db_port=${POSTGRES_PORT}
+else
+  db_host=$(echo "$DATABASE_URL" | cut -d "@" -f2 | cut -d "/" -f1 | cut -d ":" -f1)
+  db_port=$(echo "$DATABASE_URL" | cut -d "@" -f2 | cut -d "/" -f1 | cut -d ":" -f2)
+  # if port wasn't specified, use default PG port
+  if [ "$db_port" = "$db_host" ]; then
+    db_port=5432
+  fi
+fi
+
+if [ -z "${db_host}" ]; then
+  echo "No database host information found; skipping db setup"
+else
+  if [ -z "${db_port}" ]; then
+    db_port=5432
+  fi
+
+  bin/db-wait.sh "$db_host:$db_port"
+
+  if [ -z "${DATABASE_COMMAND}" ]; then
+    echo "DATABASE_COMMAND is not supplied, skipping database rake tasks"
+  else
+    for db_command in $DATABASE_COMMAND; do
+      bundle exec rake "$db_command"
+    done
+  fi
+fi
---
+++ starlight/bin/db-wait.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+host=$(printf "%s\n" "$1"| cut -d : -f 1)
+port=$(printf "%s\n" "$1"| cut -d : -f 2)
+
+while ! nc -z "$host" "$port"
+do
+  echo "waiting for $host:$port"
+  sleep 1
+done
