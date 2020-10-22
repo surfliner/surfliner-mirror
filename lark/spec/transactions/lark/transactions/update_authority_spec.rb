8b2ab7cae510bcbb8ef42b302dcee3b77884fd1b
tamsin johnson <tomjohnson@ucsb.edu>
2020-10-22 10:28:37 -0700

    lark: refactor `UpdateAuthority` tests
    in RSpec, it's better for `subject` to be the object under test, rather than the
    specific exercised code. refactor `UpdateAuthority` to use this approach, and to
    run as a stand-alone spec.

--- lark/spec/transactions/lark/transactions/update_authority_spec.rb
+++ lark/spec/transactions/lark/transactions/update_authority_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require_relative '../../../../config/environment'
 
 RSpec.describe Lark::Transactions::UpdateAuthority do
-  subject(:update) do
+  subject(:transaction) do
     described_class.new(event_stream: event_stream, adapter: adapter)
-                   .call(id: id, attributes: { pref_label: label })
   end
 
   let(:adapter) do
@@ -18,17 +18,18 @@ RSpec.describe Lark::Transactions::UpdateAuthority do
   end
 
   describe '#call' do
+    let(:attributes) { { pref_label: label } }
     let(:id) { authority.id }
     let(:label) { ['Label edited'] }
 
     it 'adds :update_attributes to the event stream' do
-      expect { update }
+      expect { transaction.call(id: id, attributes: attributes) }
         .to change { event_stream }
         .to contain_exactly have_attributes(type: :change_properties)
     end
 
     it 'returns an updated authority' do
-      expect(update.value!)
+      expect(transaction.call(id: id, attributes: attributes).value!)
         .to have_attributes(id: authority.id, pref_label: ['Label edited'])
     end
   end
