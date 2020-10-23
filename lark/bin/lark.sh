caeb1c7643b98e3ad3ff8c13e6a93d9232e738b1
tamsin johnson <tomjohnson@ucsb.edu>
2020-10-22 18:11:55 -0700

    lark: move db migrations to helm

--- lark/bin/lark.sh
+++ lark/bin/lark.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-if [ "$EVENT_ADAPTER" = "sql" ]
-then
-  while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"
-  do
-    echo "waiting for postgresql"
-    sleep 1
-  done
-
-  bundle exec rake db:migrate
-fi
-
 bundle exec puma -b tcp://0.0.0.0:5000
