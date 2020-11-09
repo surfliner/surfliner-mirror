4e343653156d835519cd9e70b5b211d2d714d749
tamsin johnson <tomjohnson@ucsb.edu>
2020-11-09 15:47:10 -0800

    lark: make rubocop happy with `QueryBasic`

--- lark/app/queries/query_basic.rb
+++ lark/app/queries/query_basic.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 ##
-# Basic class for custom queries
+# A base class with common behavior for Sequel adapter custom queries
 class QueryBasic
+  ##
+  # @!attribute [r] query_service
+  #   @return [#custom_queries]
   attr_reader :query_service
+
   delegate :connection, to: :query_service
   delegate :resource_factory, to: :query_service
 
+  ##
+  # @param query_service [#custom_queries]
   def initialize(query_service:)
     @query_service = query_service
   end
