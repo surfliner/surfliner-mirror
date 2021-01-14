40ae7c8971742a10f63e6361140925d88ad5306e
Matt Critchlow <mcritchlow@ucsd.edu>
2021-01-14 11:22:53 -0800

    starlight: file-sync.sh shouldn't require endpoint
    For AWS this is not needed, the AWS_DEFAULT_REGION is sufficient

--- scripts/file-sync.sh
+++ scripts/file-sync.sh
@@ -2,4 +2,8 @@
 set -e
 
 echo "Syncing $SOURCE_PATH files to $DESTINATION_PATH"
-aws --endpoint-url "$ENDPOINT_URL" s3 sync "$SOURCE_PATH" "$DESTINATION_PATH"
+if [ -z "$ENDPOINT_URL" ]; then
+  aws s3 sync "$SOURCE_PATH" "$DESTINATION_PATH"
+else
+  aws --endpoint-url "$ENDPOINT_URL" s3 sync "$SOURCE_PATH" "$DESTINATION_PATH"
+fi
