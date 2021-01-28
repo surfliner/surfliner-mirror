3f0cf8425d0dd79c28b571118f265ee2aac46521
Matt Critchlow <mcritchlow@ucsd.edu>
2021-01-28 08:31:31 -0800

    starlight: remove custom store_dir for Carrierwave
    This will make existing data migrations more complicated, and at the
    moment isn't actually working as expected anyway.

--- starlight/config/initializers/carrierwave_s3.rb
+++ starlight/config/initializers/carrierwave_s3.rb
@@ -3,18 +3,6 @@
 require "spotlight"
 
 if ENV["S3_BUCKET_NAME"].present?
-  module Spotlight
-    class FeaturedImageUploader < CarrierWave::Uploader::Base
-      # Overrides the default storage directory for S3/Minio uploads
-      # If we did not do this, the file path in S3 would be <bucket>/uploads/spotlight/....
-      # @return [String] Uses the image id as a folder prefix for namespacing
-      # otherwise there could easily be multiple "train.jpg" files..
-      def store_dir
-        String(model.id)
-      end
-    end
-  end
-
   CarrierWave.configure do |config|
     config.storage = :aws
     config.aws_bucket = ENV["S3_BUCKET_NAME"]
