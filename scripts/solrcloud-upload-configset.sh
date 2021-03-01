9685afb6d7b3084f49e13fe96ef34873f686da61
tamsin johnson <tomjohnson@ucsb.edu>
2021-03-01 11:20:53 -0800

    surfliner: check authentication when using solr API for config
    when we upload a solr configuration via the collections/configsets API, we want
    to ensure the configset is "trusted". to ensure this, we need authentication
    turned on.
    
    the catch here is that the bitnimi solr chart doesn't turn on authentication on
    an initial deploy until the all the nodes are up (it explictly waits for the
    other nodes to become available). this means the nodes are briefly up with
    authenication off(!) during deploy. assuming the init pod that uploads the
    configset starts up quickly, this means it will upload to an unauthenticated
    endpoint, and the configset will not be trusted.
    
    to avoid this, we ensure authentication is on before uploading the configset.

--- scripts/solrcloud-upload-configset.sh
+++ scripts/solrcloud-upload-configset.sh
@@ -15,14 +15,18 @@ while [ $COUNTER -lt 30 ]; do
   echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
   if nc -z "${SOLR_HOST}" "${SOLR_PORT}"; then
     # shellcheck disable=SC2143,SC2086
-    if curl --silent $solr_user_settings "$solr_config_list_url" | grep -q 'solrconfig'; then
+    if curl --silent --user 'fake:fake' "$solr_config_list_url" | grep -q '401'; then
+      # the solr pods come up and report available before they are ready to accept trusted configs
+      # only try to upload the config if auth is on.
+      if curl --silent $solr_user_settings "$solr_config_list_url" | grep -q 'solrconfig'; then
       echo "-- ConfigSet already exists; skipping creation ...";
-    else
-      echo "-- ConfigSet for ${CONFDIR} does not exist; creating ..."
-      # shellcheck disable=SC2035,SC2086
-      (cd "$CONFDIR" && zip -r - *) | curl -X POST $solr_user_settings --header "Content-Type:application/octet-stream" --data-binary @- "$solr_config_upload_url"
+      else
+        echo "-- ConfigSet for ${CONFDIR} does not exist; creating ..."
+        # shellcheck disable=SC2035,SC2086
+        (cd "$CONFDIR" && zip -r - *) | curl -X POST $solr_user_settings --header "Content-Type:application/octet-stream" --data-binary @- "$solr_config_upload_url"
+      fi
+      exit
     fi
-    exit
   fi
   COUNTER=$(( COUNTER+1 ));
   sleep 5s
