d99f8b6c1001496406024da03ef7ef8ad085ab2e
Matt Critchlow <mcritchlow@ucsd.edu>
2021-03-02 14:50:54 -0800

    starlight: setup Makefile dependencies for k3d
    The allows a developer to run `make deploy` and that's it.
    
    Also updates task scripts to be idempotent so they can be run again via
    make deploy and won't try and recreate things that already exist

--- starlight/scripts/deploy.sh
+++ starlight/scripts/deploy.sh
@@ -6,11 +6,27 @@ values_file=${VALUES_FILE:=scripts/k3d.yaml}
 registry_port=${REGISTRY_PORT:=41906}
 image_repository="k3d-registry.localhost:$registry_port/starlight_web"
 
-echo "Creating namespace for deployment..."
-kubectl create namespace "$namespace"
+if docker image inspect "$image_repository:local" > /dev/null 2>&1; then
+  echo "Starlight container image already exists in Registry, skipping build..."
+else
+  echo "Building and pushing Starlight container image to Registry..."
+  # shellcheck disable=SC1091
+  . ./scripts/build.sh
+fi
 
-echo "Installing Chromium into k3d cluster for running tests..."
-kubectl apply --namespace="$namespace" -f ./scripts/chromium.yaml --wait=true
+if kubectl get namespaces | grep -q "starlight-development"; then
+  echo "Namespace starlight-development already exists, skipping creation..."
+else
+  echo "Creating namespace for deployment..."
+  kubectl create namespace "$namespace"
+fi
+
+if kubectl get deployments.apps -n starlight-development | grep -q "chrome"; then
+  echo "Chromium container and service already installed, skipping creation..."
+else
+  echo "Installing Chromium into k3d cluster for running tests..."
+  kubectl apply --namespace="$namespace" -f ./scripts/chromium.yaml --wait=true
+fi
 
 echo "Ensuring Starlight Helm chart dependencies are installed..."
 helm dep up ../charts/starlight
--- starlight/scripts/setup.sh
+++ starlight/scripts/setup.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env sh
 
 port=${REGISTRY_PORT:=41906}
-echo "Creating Registry for container images..."
-k3d registry create registry.localhost --port "$port"
-echo "Creating k3s cluster for Starlight..."
-k3d cluster create starlight \
-  --api-port 6550 \
-  --registry-use k3d-registry.localhost:"$port" \
-  --servers 1 \
-  --agents 3 \
-  --port 80:80@loadbalancer \
-  --volume "$(pwd)":/src@all \
-  --wait
+if k3d registry list | grep -q "k3d-registry.localhost"; then
+  echo "Registry already exists, skipping creation..."
+else
+  echo "Creating Registry for container images..."
+  k3d registry create registry.localhost --port "$port"
+fi
+
+if k3d cluster list | grep -q "starlight"; then
+  echo "Starlight cluster already exists, skipping creation..."
+else
+  echo "Creating cluster for Starlight..."
+  k3d cluster create starlight \
+    --api-port 6550 \
+    --registry-use k3d-registry.localhost:"$port" \
+    --servers 1 \
+    --agents 3 \
+    --port 80:80@loadbalancer \
+    --volume "$(pwd)":/src@all \
+    --wait
+fi
