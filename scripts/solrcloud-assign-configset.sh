396f34fde813176b3550d25e354c55222aecf939
tom johnson <tomjohnson@ucsb.edu>
2021-03-09 17:39:31 +0000

    surfliner: collapse if/else/fi to oneliner

--- scripts/solrcloud-assign-configset.sh
+++ scripts/solrcloud-assign-configset.sh
@@ -6,11 +6,7 @@ if [ "$SOLR_ADMIN_USER" ]; then
   solr_user_settings="--user $SOLR_ADMIN_USER:$SOLR_ADMIN_PASSWORD"
 fi
 
-if [ "$SOLR_CONFIG_NAME"]; then
-  solr_config_name="$SOLR_CONFIG_NAME"
-else
-  solr_config_name="solrconfig"
-fi
+solr_config_name="${SOLR_CONFIG_NAME:-solrconfig}"
 
 # Solr Cloud Collection API URLs
 solr_collection_list_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=LIST"
