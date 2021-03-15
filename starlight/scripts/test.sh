a385d80ff60a477d172e5baed485fca83b4989ca
Matt Critchlow <mcritchlow@ucsd.edu>
2021-03-15 11:00:08 -0700

    starlight: support test arguments in Makefile
    This allows you to run individual, or isolated tests using the Makefile.
    
    Example: `TEST_ARGS="spec/models/user_spec.rb" make test`
    
    This can be incorporated as a script and then configured with an editor
    to run tests in various contexts as needed.

--- starlight/scripts/test.sh
+++ starlight/scripts/test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
-# TODO: support passing rspec arguments for individual test runs, etc.
 pod="$(kubectl get pods --namespace=starlight-development "--selector=app.kubernetes.io/name=starlight" --no-headers | cut -d " " -f 1)"
 
-kubectl exec -it --namespace=starlight-development "$pod" -- /bin/sh -c "bundle exec rake"
+kubectl exec -it --namespace=starlight-development "$pod" -- /bin/sh -c "bundle exec rspec $*"
 
