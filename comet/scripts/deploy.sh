1dc347432d93a982f69bb6d8dadcda9f67ad8d5c
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-01 09:52:38 -0700

    comet: add deploy to make dev environment
    a very fast pass at porting the deploy script.
    
    since there's no published Hyrax helm chart yet, this assumes you have a hyrax
    checkout on your local file system. :(

---
+++ comet/scripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+namespace="comet-development"
+release=${RELEASE_NAME:=comet}
+values_file=${VALUES_FILE:=scripts/k3d.yaml}
+registry_port=${REGISTRY_PORT:=41906}
+image_repository="k3d-registry.localhost:$registry_port/comet_web"
+
+if docker image inspect "$image_repository:local" > /dev/null 2>&1; then
+  echo "Comet container image already exists in Registry, skipping build..."
+else
+  echo "Building and pushing Comet container image to Registry..."
+  # shellcheck disable=SC1091
+  . ./scripts/build.sh
+fi
+
+if kubectl get namespaces | grep -q $namespace; then
+  echo "Namespace $namespace already exists, skipping creation..."
+else
+  echo "Creating namespace for deployment..."
+  kubectl create namespace "$namespace"
+fi
+
+if kubectl get deployments.apps -n $namespace | grep -q "chrome"; then
+  echo "Chromium container and service already installed, skipping creation..."
+else
+  echo "Installing Chromium into k3d cluster for running tests..."
+  kubectl apply --namespace="surfliner-utilities" -f ../k3d/chromium.yaml --wait=true
+fi
+
+helm dep up ../../hyrax/chart/hyrax
+
+echo "Deploying Hyrax using Helm chart into k3d cluster..."
+helm upgrade \
+  --install \
+  --namespace="$namespace" \
+  --set image.repository="$image_repository" \
+  --set worker.image.repository="$image_repository" \
+  --values="$values_file" \
+  "$release" \
+  ../../hyrax/chart/hyrax
