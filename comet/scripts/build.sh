0dcde267d4bcbf61f5ee1d279e2cc1ffde24c647
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-02 10:31:05 -0700

    comet: use the surfliner root as the docker build context
    the Dockerfile got changed to expect this, but the build script was never
    updated. it didn't emerge as an issue because we all had layers cached, but
    changing `Gemfile` breaks the build.

--- comet/scripts/build.sh
+++ comet/scripts/build.sh
@@ -5,7 +5,7 @@ git_sha="$(git rev-parse HEAD)"
 image_tag="k3d-registry.localhost:$registry_port/comet_web:${git_sha}"
 
 echo "Building Comet image..."
-docker build -t "$image_tag" -f Dockerfile .
+docker build -t "$image_tag" -f Dockerfile ..
 
 echo "Pushing Comet image to local registry..."
 docker push "$image_tag"
