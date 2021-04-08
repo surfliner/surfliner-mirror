fcffe68c6eabd4caaf297aa561355c15065cfa4f
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-08 12:54:26 -0700

    comet: setup admin user seeds
    create user seeds for database admin users. these email addresses are already in
    the role map config.

--- comet/db/seeds.rb
+++ comet/db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  puts "\n== Creating development admin users"
+  ucsb = User.where(email: "comet-admin@library.ucsb.edu").first_or_create do |f|
+    f.password = "admin_password"
+  end
+
+  ucsd = User.where(email: "comet-admin@library.ucsd.edu").first_or_create do |f|
+    f.password = "admin_password"
+  end
+
+  [ucsb, ucsd].each { |user| puts "\nAdmin user: #{user.user_key}:admin_password" }
+end
