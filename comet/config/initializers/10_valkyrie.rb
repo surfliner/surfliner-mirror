e976b7c70c082934ffa88348e2ca29c2a054e574
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-09 11:21:54 -0700

    comet: register a valkyrie sequel adapter

---
+++ comet/config/initializers/10_valkyrie.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+unless ENV["DB_ADAPTER"] == "nulldb" || Rails.env.test?
+  database = ENV.fetch("METADATA_DATABASE_NAME", "comet_metadata")
+  Rails.logger.info "Establishing connection to postgresql on: " \
+                    "#{ENV["DB_HOST"]}:#{ENV["DB_PORT"]}.\n" \
+                    "Using database: #{database}."
+  connection = Sequel.connect(
+    user: ENV["DB_USERNAME"],
+    password: ENV["DB_PASSWORD"],
+    host: ENV["DB_HOST"],
+    port: ENV["DB_PORT"],
+    database: database,
+    max_connections: ENV.fetch("METADATA_DATABASE_POOL", 5),
+    pool_timeout: ENV.fetch("METADATA_DATABASE_TIMEOUT", 5),
+    adapter: :postgres
+  )
+
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Sequel::MetadataAdapter.new(connection: connection), :comet_metadata_store
+  )
+end
