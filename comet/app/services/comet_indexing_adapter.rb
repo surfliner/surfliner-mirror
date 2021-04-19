256a5579cffe59530f06553c81991295ce0e8785
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-19 16:27:38 -0700

    comet: build solr url in application; avoid `SOLR_URL`
    the Hyrax chart has a helper for constructing the SOLR_URL. this seems like a
    good idea at first blush, but it falls apart when the solr password is provided
    by an external secret (and therefore not available in the chart).
    
    we probably want a resolution for this on the Hyrax side. deprecating SOLR_URL
    completely seems appropriate. in the meanwhile we should avoid its use here.

--- comet/app/services/comet_indexing_adapter.rb
+++ comet/app/services/comet_indexing_adapter.rb
@@ -5,6 +5,6 @@
 # authentication.
 class CometIndexingAdapter < Valkyrie::Indexing::Solr::IndexingAdapter
   def connection_url
-    ENV.fetch("SOLR_URL") { super }
+    sprintf "http://%s:%s@%s:%s/solr/%s", ENV["SOLR_ADMIN_USER"], ENV["SOLR_ADMIN_PASSWORD"], ENV["SOLR_HOST"], ENV["SOLR_PORT"], ENV["SOLR_COLLECTION_NAME"]
   end
 end
