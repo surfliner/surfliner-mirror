b6a2b9026fb493fbe343f8e4d1a54ae259882468
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-20 08:44:06 -0700

    comet: remove reference to FCREPO ENV variables
    these variables aren't needed; we should never wait for Fedora in Comet.

--- comet/scripts/db-migrate-seed.sh
+++ comet/scripts/db-migrate-seed.sh
@@ -6,9 +6,6 @@ bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec sequel -m db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
 
-if [ "$FCREPO_HOST" ]; then
-  db-wait.sh "$FCREPO_HOST:$FCREPO_PORT"
-fi
 db-wait.sh "$SOLR_HOST:$SOLR_PORT"
 
 bundle exec rails db:seed
