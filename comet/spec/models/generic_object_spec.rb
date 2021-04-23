cb9db04032ab8b038ded2f2bdcb41068df61b37d
tamsin johnson <tomjohnson@ucsb.edu>
2021-04-22 17:16:51 -0700

    comet: don't allow Valkyrie::Sequel to filter by UUID
    this constant has a proposed removal at
    https://github.com/samvera-labs/valkyrie-sequel/pull/38.
    
    it is used to do mass client side filtering of ids. the proposed upstream fix is
    to delegate this resposibility to Postgresql, which can use its own schema to
    decide on allowed syntax.

--- comet/spec/models/generic_object_spec.rb
+++ comet/spec/models/generic_object_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe GenericObject do
   subject(:work) { described_class.new }
 
   it_behaves_like "a Hyrax::Work"
+
+  it "round trips the work" do
+    work.title = "Comet in Moominland"
+    id = Hyrax.persister.save(resource: work).id
+
+    expect(Hyrax.query_service.find_by(id: id))
+      .to have_attributes(title: contain_exactly("Comet in Moominland"))
+  end
 end
