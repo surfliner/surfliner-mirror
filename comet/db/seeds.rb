55062c4783c96c2390f4e6858f68225d9b66e61b
Longshou Situ <lsitu@ucsd.edu>
2021-07-14 13:26:44 -0700

    Comet: Load default workflow for development environment.

--- comet/db/seeds.rb
+++ comet/db/seeds.rb
@@ -14,6 +14,14 @@ if Rails.env.development?
 
   puts "\n== Creating default Project (AdministrativeSet)"
   project = Hyrax::AdministrativeSet.new(title: "Default Project")
-  Hyrax.persister.save(resource: project)
+  project = Hyrax.persister.save(resource: project)
   Hyrax.index_adapter.save(resource: project)
+
+  puts "\n== Creating PermissionTemplate for default AdministrativeSet #{project.id}"
+  Hyrax::PermissionTemplate.find_or_create_by!(source_id: project.id.to_s)
+
+  puts "\n== Loading workflows"
+  Hyrax::Workflow::WorkflowImporter.load_workflows
+  errors = Hyrax::Workflow::WorkflowImporter.load_errors
+  puts "Failed to process all workflows:\n  #{errors.join('\n  ')}" unless errors.empty?
 end
