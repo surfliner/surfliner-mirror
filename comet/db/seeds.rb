3b3af7c62fc2a7b4fd8a9b8d87db24188ff943ee
Matt Critchlow <mcritchlow@ucsd.edu>
2021-07-15 11:40:03 -0700

    comet: assign worklow roles to admin users in dev
    In the development environment, we probably want to have our admin users
    automatically assigned to all workflow roles. Otherwise, for testing, we
    would need to manually do this in the Admin UI.

--- comet/db/seeds.rb
+++ comet/db/seeds.rb
@@ -1,18 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-if Rails.env.development?
-  provider = ENV["AUTH_METHOD"]
-  puts "\n== Creating development admin users"
-  ucsb = User.find_or_create_by(email: "comet-admin@library.ucsb.edu",
-    provider: provider)
-
-  ucsd = User.find_or_create_by(email: "comet-admin@library.ucsd.edu",
-    provider: provider)
-
-  [ucsb, ucsd].each { |user| puts "\nAdmin user email: #{user.user_key}" }
-end
-
 projects = Hyrax.query_service.find_all_of_model(model: Hyrax::AdministrativeSet)
 
 if projects.none?
@@ -24,8 +12,26 @@ if projects.none?
   puts "\n== Creating PermissionTemplate for default AdministrativeSet #{project.id}"
   Hyrax::PermissionTemplate.find_or_create_by!(source_id: project.id.to_s)
 
+  # TODO: feature spec: create a work assigned to workflow, log in as reviewer, approve or take some action
   puts "\n== Loading workflows"
-  Hyrax::Workflow::WorkflowImporter.load_workflows
-  errors = Hyrax::Workflow::WorkflowImporter.load_errors
-  puts "Failed to process all workflows:\n  #{errors.join('\n  ')}" unless errors.empty?
+  Rake::Task["hyrax:workflow:load"].execute
+end
+
+if Rails.env.development?
+  provider = ENV["AUTH_METHOD"]
+  puts "\n== Creating development admin users"
+  ucsb = User.find_or_create_by(email: "comet-admin@library.ucsb.edu",
+    provider: provider)
+
+  ucsd = User.find_or_create_by(email: "comet-admin@library.ucsd.edu",
+    provider: provider)
+
+  [ucsb, ucsd].each { |user| puts "\nAdmin user email: #{user.user_key}" }
+
+  puts "\n== Assigning all workflow roles to admin users"
+  [ucsb, ucsd].each { |user| user.to_sipity_agent }
+  Sipity::WorkflowRole.all.each do |wf_role|
+    Sipity::WorkflowResponsibility.find_or_create_by!(agent_id: ucsb.sipity_agent.id, workflow_role_id: wf_role.role_id)
+    Sipity::WorkflowResponsibility.find_or_create_by!(agent_id: ucsd.sipity_agent.id, workflow_role_id: wf_role.role_id)
+  end
 end
