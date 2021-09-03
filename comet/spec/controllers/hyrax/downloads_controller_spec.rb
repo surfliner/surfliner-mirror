d75be4e21840c05482678bca3f58f82ba010b061
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-02 17:06:05 -0700

    comet: use the test adapters to speed up some slower tests
    the downloads controller unit specs shouldn't need to hit the real s3
    backend. use the memory adapter instead.

--- comet/spec/controllers/hyrax/downloads_controller_spec.rb
+++ comet/spec/controllers/hyrax/downloads_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe Hyrax::DownloadsController do
+RSpec.describe Hyrax::DownloadsController, storage_adapter: :memory, metadata_adapter: :test_adapter do
   routes { Hyrax::Engine.routes }
   let(:user) { User.create(email: "moomin@example.com") }
   let(:file_set) { Hyrax.persister.save(resource: Hyrax::FileSet.new) }
@@ -19,7 +19,7 @@ RSpec.describe Hyrax::DownloadsController do
         sign_in user
         allow(controller).to receive(:authorize!).with(:download, file_set.id).and_return true
         file_set.file_ids << upload.id
-        Hyrax.persister.save(resource: file_set)        
+        Hyrax.persister.save(resource: file_set)
         get :show, params: {id: file_set.id}
         file.rewind
         expect(response.body).to eq file.read
