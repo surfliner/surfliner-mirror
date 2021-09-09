0fa3779f75154c10a3bf209a7877c960bab6ff6d
tamsin johnson <tomjohnson@ucsb.edu>
2021-09-09 12:08:10 -0700

    comet: use `WorkflowRole#id` as the workflow_role_id argument
    `WorkflowResponsibility` wants a reference to a `WorkflowRole` (NOT a
    `Role`). we were prevously passing `WorkflowRole#role_id` in as this
    argument. this works ONLY in the case tha the `WorkflowRole` and `Role` objects
    were created 1:1, sequentially, so their ids match.
    
    that's not an assumption we can count on. in the case where it fails, we get
    errors from ActiveRecord, or worse, just incorrect refernces to the wrong
    roles.

--- comet/spec/support/workflow.rb
+++ comet/spec/support/workflow.rb
@@ -27,7 +27,7 @@ def setup_workflow_for(user)
 
   puts "\n== Assigning all workflow roles to #{user}"
   Sipity::WorkflowRole.all.each do |wf_role|
-    Sipity::WorkflowResponsibility.find_or_create_by!(agent_id: user.to_sipity_agent.id, workflow_role_id: wf_role.role_id)
+    Sipity::WorkflowResponsibility.find_or_create_by!(agent_id: user.to_sipity_agent.id, workflow_role_id: wf_role.id)
   end
 
   puts "\n== Assigning permissions to #{user}"
