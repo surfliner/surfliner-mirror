5d2077e13599b3ff850a40e7d6ba9ec1e5ea3a7b
Matt Critchlow <mcritchlow@ucsd.edu>
2021-09-14 15:28:51 -0700

    comet: allow send original file spec to pass
    We needed to setup the FileMetadata configuration for the FileSet and
    properly associated the two, in order for the Hyrax.custom_queries that
    are invoked in the DownloadsController to work

--- comet/spec/controllers/hyrax/downloads_controller_spec.rb
+++ comet/spec/controllers/hyrax/downloads_controller_spec.rb
@@ -5,6 +5,7 @@ require "rails_helper"
 RSpec.describe Hyrax::DownloadsController, storage_adapter: :memory, metadata_adapter: :test_adapter do
   routes { Hyrax::Engine.routes }
   let(:user) { User.create(email: "moomin@example.com") }
+  let(:file_set_metadata) { Hyrax::FileMetadata.for(file: file) }
   let(:file_set) { Hyrax.persister.save(resource: Hyrax::FileSet.new) }
   let(:file) { Tempfile.new("moomin.jpg").tap { |f| f.write("def") } }
   let(:upload) { Hyrax.storage_adapter.upload(resource: file_set, file: file, original_filename: "Moomin.jpg") }
@@ -16,11 +17,18 @@ RSpec.describe Hyrax::DownloadsController, storage_adapter: :memory, metadata_ad
     end
 
     context "when the file exists" do
-      xit "sends the original file" do
+      it "sends the original file" do
         sign_in user
         allow(controller).to receive(:authorize!).with(:download, file_set.id).and_return true
+        allow(file).to receive(:original_filename).and_return("Moomin.jpg")
+        allow(file).to receive(:content_type).and_return("image/jpeg")
         file_set.file_ids << upload.id
         Hyrax.persister.save(resource: file_set)
+        file_set_metadata.file_identifier = upload.id
+        file_set_metadata.file_set_id = file_set.id
+        file_set_metadata.size = upload.size
+        file_set_metadata.id = upload.id
+        Hyrax.persister.save(resource: file_set_metadata)
         get :show, params: {id: file_set.id}
         file.rewind
         expect(response.body).to eq file.read
