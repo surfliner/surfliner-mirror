947939bc1cbdf9be678470a56ea5bf931eb687ff
Matt Critchlow <mcritchlow@ucsd.edu>
2021-09-16 11:48:19 -0700

    comet: collections_controller_spec for create
    This creates an initial test in the CollectionsController for the
    `#create` action.

--- comet/spec/controllers/hyrax/dashboard/collections_controller_spec.rb
+++ comet/spec/controllers/hyrax/dashboard/collections_controller_spec.rb
@@ -25,6 +25,29 @@ RSpec.describe Hyrax::Dashboard::CollectionsController, storage_adapter: :memory
     end
   end
 
+  describe "#create" do
+    it "redirects to sign in" do
+      post :create, params: { pcdm_collection: { title: ["my title"] } }
+
+      expect(response).to redirect_to("/users/sign_in")
+    end
+
+    context "with a logged in user" do
+      before { sign_in(user) }
+
+      let(:collection_type) { Hyrax::CollectionType.create(title: "Spec Type") }
+
+      it "gives a succesful response" do
+        expect do
+          post :create, params: { pcdm_collection: { title: ["my title"] },
+                                collection_type_gid: collection_type.to_global_id }
+        end
+          .to change { Hyrax.query_service.count_all_of_model(model: Hyrax::PcdmCollection) }
+          .by 1
+      end
+    end
+  end
+
   describe "#show" do
     let(:collection) do
       c = Hyrax::PcdmCollection.new(title: ["My Collection"])
