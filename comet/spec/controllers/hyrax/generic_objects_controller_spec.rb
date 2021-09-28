163af38ab22f8ee058c1c46d63801ea6c6ebb896
Matt Critchlow <mcritchlow@ucsd.edu>
2021-09-28 10:35:52 -0700

    comet: add initial generic_objects_controller_spec
    For now we're trying to get a test harness around associating
    collections with a GenericObject.

---
+++ comet/spec/controllers/hyrax/generic_objects_controller_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Hyrax::GenericObjectsController, storage_adapter: :memory, metadata_adapter: :test_adapter do
+  let(:user) { User.create(email: "moomin@example.com") }
+
+  describe "#create" do
+    context "when assigning a collection relationship" do
+      let(:collection_type) { Hyrax::CollectionType.create(title: "Spec Type") }
+      let(:collection) { Hyrax::PcdmCollection.new(title: "Spec Type",
+                                                   collection_type_gid: collection_type.to_global_id) }
+
+      it "persists the collection id for the object" do
+        sign_in user
+        collection_id = Hyrax.persister.save(resource: collection).id
+        post :create, params: { generic_object: { title: "Test Object",
+                                member_of_collection_ids: collection_id.to_s}}
+
+        persisted_object = Hyrax.query_service.find_all_of_model(model: GenericObject).first
+        expect(persisted_object.member_of_collection_ids).to eq([collection_id])
+      end
+    end
+  end
+end
