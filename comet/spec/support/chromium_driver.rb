cc96c483de6caab45bf6a7de13dedafe4fa5e511
Matt Critchlow <mcritchlow@ucsd.edu>
2021-09-30 13:14:47 -0700

    comet: relax CAPYBARA_WAIT_TIME to 10s
    We don't have consistently fast dev/CI environments. So we should allow
    for some delay in `find`'s and such.

--- comet/spec/support/chromium_driver.rb
+++ comet/spec/support/chromium_driver.rb
@@ -13,3 +13,5 @@ end
 
 Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
 Capybara.server_port = ENV["CAPYBARA_PORT"]
+Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_WAIT_TIME', 10) # We may have a slow application, let's give it some time.
+
