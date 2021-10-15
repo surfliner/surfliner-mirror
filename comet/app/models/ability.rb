cecbd6a6fa3fd0860004119a23a8be0903ce3b9c
Matt Critchlow <mcritchlow@ucsd.edu>
2021-10-15 10:31:47 -0700

    comet: remove custom collection ability setup
    This is now handled upstream!

--- comet/app/models/ability.rb
+++ comet/app/models/ability.rb
@@ -4,17 +4,6 @@ class Ability
 
   self.ability_logic += [:everyone_can_create_curation_concerns]
 
-  # Define any customized permissions here.
-  def custom_permissions
-    can :read, Hyrax::PcdmCollection do |collection|
-      test_read(collection.id)
-    end
-
-    can :deposit, Hyrax::PcdmCollection do |collection|
-      Hyrax::Collections::PermissionsService.can_deposit_in_collection?(ability: self, collection_id: collection.id.to_s)
-    end
-  end
-
   ##
   # @note just assume everyone can deposit works; what are they here for
   #   otherwise? anyway, we imagine that the reasons folks might not have
