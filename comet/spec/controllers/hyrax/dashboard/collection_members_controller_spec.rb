01d2e55467cd921695e508290e2770acd430fcea
tamsin johnson <tomjohnson@ucsb.edu>
2021-10-27 14:27:03 -0700

    WIP: failing spec; probably need to make collection member readable as next step

--- comet/spec/controllers/hyrax/dashboard/collection_members_controller_spec.rb
+++ comet/spec/controllers/hyrax/dashboard/collection_members_controller_spec.rb
@@ -2,15 +2,18 @@
 
 require "rails_helper"
 RSpec.describe Hyrax::Dashboard::CollectionMembersController, storage_adapter: :memory, metadata_adapter: :test_adapter, type: :controller do
-
   routes { Hyrax::Engine.routes }
-  let(:user) { User.create(email: "moomin@example.com") }
 
-  before { sign_in(user) }
+  let(:collection) do
+    Hyrax.persister.save(resource: Hyrax::PcdmCollection.new(title: ['Test Collection'],
+                                                             collection_type_gid: collection_type_gid))
+  end
 
-  let(:collection) { Hyrax::PcdmCollection.new(title: ['Test Collection'], collection_type_gid: collection_type_gid )
   let(:collection_type) { Hyrax::CollectionType.create(title: "Spec Type") }
   let(:collection_type_gid) { collection_type.to_global_id.to_s }
+  let(:user) { User.create(email: "moomin@example.com") }
+
+  before { sign_in(user)}
 
   describe '#update_members' do
     let(:members_to_add) { [collection_member] }
@@ -28,7 +31,7 @@ RSpec.describe Hyrax::Dashboard::CollectionMembersController, storage_adapter: :
 
     it 'can add a collection member to the collection' do
       expect { post :update_members, params: parameters }
-        .to change { queries.find_members_of(collection: collection.valkyrie_resource).map(&:id) }
+        .to change { Hyrax.custom_queries.find_members_of(collection: collection).map(&:id) }
         .from(be_empty)
         .to contain_exactly(collection_member)
     end
