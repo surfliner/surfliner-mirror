4204c3518859415296025d290815b00a7b906703
VivianChu <tchu@ucsd.edu>
2021-10-28 15:48:06 -0700

    Fix failed test

--- comet/spec/controllers/hyrax/dashboard/collection_members_controller_spec.rb
+++ comet/spec/controllers/hyrax/dashboard/collection_members_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Hyrax::Dashboard::CollectionMembersController, storage_adapter: :
 
   let(:collection_type) { Hyrax::CollectionType.create(title: "Spec Type") }
   let(:collection_type_gid) { collection_type.to_global_id.to_s }
-  let(:user) { User.create(email: "moomin@example.com") }
+  let(:user) { User.create(email: "comet-admin@library.ucsd.edu") }
 
   before { sign_in(user)}
 
@@ -25,15 +25,21 @@ RSpec.describe Hyrax::Dashboard::CollectionMembersController, storage_adapter: :
     end
 
     let(:collection_member) do
-      Hyrax::PcdmCollection.new(title: ['Test Collection'],
-                                collection_type_gid: collection_type_gid )
+      Hyrax.persister.save(resource: Hyrax::PcdmCollection.new(title: ['Test Sub Collection'],
+                                                             collection_type_gid: collection_type_gid))
+                                
     end
 
     it 'can add a collection member to the collection' do
+      Hyrax::Collections::PermissionsCreateService.create_default(collection: collection, creating_user: user)
+      collection.permission_manager.read_users += [user.user_key]
+      collection.permission_manager.edit_users += [user.user_key]
+      collection.permission_manager.acl.save
+      Hyrax.index_adapter.save(resource: collection)
       expect { post :update_members, params: parameters }
         .to change { Hyrax.custom_queries.find_members_of(collection: collection).map(&:id) }
         .from(be_empty)
-        .to contain_exactly(collection_member)
+        .to contain_exactly(collection_member.id)
     end
   end
 end
