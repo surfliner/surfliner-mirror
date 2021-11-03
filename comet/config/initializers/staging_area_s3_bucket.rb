65fe12693df5480104f5a2c51c3e089fb66b6901
Matt Critchlow <mcritchlow@ucsd.edu>
2021-11-03 08:51:11 -0700

    comet: don't create STAGING_AREA_S3_BUCKET
    This code presumes a running Minio instance. In an initializer, this is
    probably not a great idea, unless we want to loop and wait until we're
    sure there's a connection.
    
    The pattern we've used elsewhere is to either:
    - have an initContainer or separate task responsible for setting up
      thigns like buckets.
    - assume the deployer is responsible for ensuring buckets required for
      an application already exist
    
    For docker compose and review apps we have a solution for this already
    in place

--- comet/config/initializers/staging_area_s3_bucket.rb
+++ comet/config/initializers/staging_area_s3_bucket.rb
@@ -26,15 +26,7 @@ unless ENV["DB_ADAPTER"] == "nulldb" || aws_access_key_id.nil? || aws_secret_acc
   fog_connection = Fog::Storage.new(provider: "AWS", **fog_connection_options)
   Rails.application.config.staging_area_s3_connection = fog_connection
 
-  # Create the bucket if it doesn't exist
   s3_bucket = ENV.fetch("STAGING_AREA_S3_BUCKET", "comet-staging-area-#{Rails.env}")
-  begin
-    puts "-- Using staging area bucket: #{s3_bucket}"
-    fog_connection.head_bucket(s3_bucket)
-  rescue Excon::Error::NotFound
-    puts "-- Creating buchet #{s3_bucket}"
-    fog_connection.put_bucket(s3_bucket)
-  end
 
   Rails.application.config.staging_area_s3_handler =
     StagingAreaS3Handler.new(connection: fog_connection,
