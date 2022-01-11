d7c0107b2ee7bfab2add2fdad5176e5dc166d3b2
Thor Brickman <tnbrickman@ucsd.edu>
2022-01-11 14:55:53 -0800

    Sheesh...the csv is a mess...

--- docs/ProcessAndCulture/processretriumcsv.sh
+++ docs/ProcessAndCulture/processretriumcsv.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Just a placeholder for now.
 DUMP=`cat actionitems.csv`
 
 while IFS= read -r line; do
     echo "${line:0:1}"
     if [ "${line:0:1}" == '"' ]; then
-	echo "Found a quote"
+	    echo "Found a quote"
+        # Since the csv is malformed, we need to look for ", before we can grab the rest.
+        CURRENT_CHAR=0
+        while [ "${line:$CURRENT_CHAR:1}" != '' ]; do
+            echo -n ${line:$CURRENT_CHAR:1}
+            ((CURRENT_CHAR=CURRENT_CHAR+1))
+        done
     else
-	echo "Didn't find a quote"
+	    echo "Didn't find a quote"
     fi
 done <<< "$DUMP"
 
