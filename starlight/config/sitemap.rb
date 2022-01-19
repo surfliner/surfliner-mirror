e51859532656f75f0f8182ef589f19a4c197b705
Matt Critchlow <mcritchlow@ucsd.edu>
2022-01-19 10:57:21 -0800

    starlight: support minio in sitemap configuration
    With the upstream changes introduced in 6.2.0 of the sitemap_generator
    gem, we can directly pass the additional options needed by Mino along to
    the setup for sitemaps, instead of our previous `class_eval` solution

--- starlight/config/sitemap.rb
+++ starlight/config/sitemap.rb
@@ -4,42 +4,20 @@ if ENV["SITEMAPS_ENABLED"].present?
   require "aws-sdk-s3"
   require "sitemap_generator"
 
-  if ENV["S3_ENDPOINT"].present?
-    # Horrible hack to get Minio working for now
-    # Minio requires the force_path_style option to be set
-    # see: https://github.com/kjvarga/sitemap_generator/pull/351
-    SitemapGenerator::AwsSdkAdapter.class_eval do
-      def s3_resource_options
-        options = {}
-        options[:force_path_style] = true # Minio requirement
-        options[:region] = @aws_region if !@aws_region.nil?
-        options[:endpoint] = @aws_endpoint if !@aws_endpoint.nil?
-        if !@aws_access_key_id.nil? && !@aws_secret_access_key.nil?
-          options[:credentials] = Aws::Credentials.new(
-            @aws_access_key_id,
-            @aws_secret_access_key
-          )
-        end
-        options
-      end
-      # rubocop:enable Style/NegatedIf
+  minio_config_options =
+    if ENV["S3_ENDPOINT"].present?
+      {aws_endpoint: ENV["S3_ENDPOINT"], force_path_style: true}
+    else
+      {}
     end
 
-    SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-      ENV["S3_BUCKET_NAME"],
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      aws_region: ENV["AWS_REGION"],
-      aws_endpoint: ENV["S3_ENDPOINT"]
-    )
-  else
-    SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
-      ENV["S3_BUCKET_NAME"],
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      aws_region: ENV["AWS_REGION"]
-    )
-  end
+  SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+    ENV["S3_BUCKET_NAME"],
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    aws_region: ENV["AWS_REGION"],
+    **minio_config_options
+  )
 
   SitemapGenerator::Sitemap.default_host = ENV.fetch("APP_URL")
 
