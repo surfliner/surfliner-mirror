7583a890cae2fd80f169c9b93ed6c3a4a8bcd848
kibigo! <kibigo@ucsb.edu>
2022-01-21 14:39:12 -0800

    tidewater: allow for multiple of the same term
    • Use U+FFFF to delimit multiple of the same term
    • Add metadata_for method for getting metadata as an array
    • Validate that terms are representable in XML
    • Simple tests for the above

--- tidewater/app/models/oai_item.rb
+++ tidewater/app/models/oai_item.rb
@@ -1,2 +1,43 @@
 class OaiItem < ApplicationRecord
+
+  # The fifteen elements defined in the original Dublin Core 1.1 Elements namespace, which are stored as fields in our database.
+  def self.dc_elements
+    OAI::Provider::Metadata::DublinCore.instance.fields
+  end
+
+  # The fields in our database which contain XML (potentially delimited with U+FFFF).
+  def self.xml_fields
+    self.dc_elements
+  end
+
+  # Ensure that each of the passed fields does not contain any characters not allowed in XML 1.0, save U+FFFF, which is used to delimit multiple values.
+  validates *self.xml_fields, format: {
+    without: /[\x00-\x08\x0B\x0C\x0E-\x1F\uFFFE]/,
+    message: "must be representable in XML"
+  }
+
+  # Gets the metadata associated with a given URI, if such metadata exists in the database.
+  #
+  # Always returns an array of strings. If no term is found, the array will be empty.
+  #
+  # @param uri [String] the URI for the metadata term
+  # @return [Array<String>] defined values for the metadata term
+  def metadata_for(uri)
+    if uri.start_with? "http://purl.org/dc/elements/1.1/"
+      # This URI is in the Dublin Core elements namespace.
+      field = uri.delete_prefix("http://purl.org/dc/elements/1.1/").to_sym
+      if self.class.dc_elements.include? field
+        # This is a defined Dublin Core element; get it from the model.
+        # Dublin Core elements are stored directly in the database as strings, with multiple instances delimited by "\uFFFF".
+        return self.send(field).to_s.split("\uFFFF", -1)
+      else
+        # This is not a defined Dublin Core element.
+        return []
+      end
+    else
+      # TODO: Other kinds of metadata terms?
+      return []
+    end
+  end
+
 end
--- tidewater/spec/models/oai_item_spec.rb
+++ tidewater/spec/models/oai_item_spec.rb
@@ -1,8 +1,36 @@
 require "rails_helper"
 
 RSpec.describe OaiItem do
-  it "has a title" do
-    subject.title = "foo"
-    expect(subject.title).to eq "foo"
+  it 'does not allow most characters disallowed in XML' do
+    ["\x00", "\x12", "\uFFFE"].each do |badchar|
+      subject.title = "foo#{badchar}bar"
+      expect(subject.valid?).to be false
+    end
+  end
+
+  it 'allows some special characters allowed in XML' do
+    ["\x09", "\x0A", "\x20", "\x7F", "\u0085"].each do |goodchar|
+      subject.title = "foo#{goodchar}bar"
+      expect(subject.valid?).to be true
+    end
+  end
+
+  it 'allows U+FFFF' do
+    subject.contributor = "Etaoin Shrdlu\uFFFFCmfwyp Vbgkqj"
+    expect(subject.valid?).to be true
+  end
+
+  it 'returns fields with no value as an empty array' do
+    expect(subject.metadata_for 'http://purl.org/dc/elements/1.1/contributor').to eq []
+  end
+
+  it 'returns fields with one value as an array with one element' do
+    subject.contributor = 'Etaoin Shrdlu'
+    expect(subject.metadata_for 'http://purl.org/dc/elements/1.1/contributor').to eq ['Etaoin Shrdlu']
+  end
+
+  it 'returns fields with multiple values as an array of values' do
+    subject.contributor = "Etaoin Shrdlu\uFFFFCmfwyp Vbgkqj"
+    expect(subject.metadata_for 'http://purl.org/dc/elements/1.1/contributor').to eq ['Etaoin Shrdlu', 'Cmfwyp Vbgkqj']
   end
 end
