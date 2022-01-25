275802fdab8c8b3b35c8cc15d73d10ff96644d26
kibigo! <kibigo@ucsb.edu>
2022-01-24 17:03:04 -0800

    tidewater: replace bad characters with U+FFFD
    Instead of validating, since it looks as though database data might
      be added through mechanisms other than Rails.

--- tidewater/app/models/oai_item.rb
+++ tidewater/app/models/oai_item.rb
@@ -1,20 +1,17 @@
+# An OAI-PMH item.
+#
+# OAI-PMH records can be generated using the `to_{metadata_prefix}` methods.
+# <https://github.com/code4lib/ruby-oai/blob/3cdc12d86c7d1dde00b47105a7dab6def3f6801d/lib/oai/provider.rb#L209-L232>.
 class OaiItem < ApplicationRecord
-
-  # The fifteen elements defined in the original Dublin Core 1.1 Elements namespace, which are stored as fields in our database.
+  # The fifteen elements defined in the original Dublin Core 1.1 Elements namespace, stored as fields in our database.
   def self.dc_elements
     OAI::Provider::Metadata::DublinCore.instance.fields
   end
 
   # The fields in our database which contain XML (potentially delimited with U+FFFF).
   def self.xml_fields
-    self.dc_elements
+    dc_elements
   end
-
-  # Ensure that each of the passed fields does not contain any characters not allowed in XML 1.0, save U+FFFF, which is used to delimit multiple values.
-  validates *self.xml_fields, format: {
-    without: /[\x00-\x08\x0B\x0C\x0E-\x1F\uFFFE]/,
-    message: "must be representable in XML"
-  }
 
   # Gets the metadata associated with a given URI, if such metadata exists in the database.
   #
@@ -26,18 +23,26 @@ class OaiItem < ApplicationRecord
     if uri.start_with? "http://purl.org/dc/elements/1.1/"
       # This URI is in the Dublin Core elements namespace.
       field = uri.delete_prefix("http://purl.org/dc/elements/1.1/").to_sym
-      if self.class.dc_elements.include? field
-        # This is a defined Dublin Core element; get it from the model.
-        # Dublin Core elements are stored directly in the database as strings, with multiple instances delimited by "\uFFFF".
-        return self.send(field).to_s.split("\uFFFF", -1)
-      else
-        # This is not a defined Dublin Core element.
-        return []
-      end
+      return [] unless self.class.dc_elements.include? field
+
+      # This is a defined Dublin Core element; get it from the model.
+      # Dublin Core elements are stored directly in the database as strings, with multiple values delimited by U+FFFF.
+      values_from_field(send(field).to_s)
     else
       # TODO: Other kinds of metadata terms?
-      return []
+      []
     end
   end
 
+  private
+
+  # Splits the provided text on U+FFFF and replaces any characters not allowed in XML documents with U+FFFD.
+  #
+  # @param text [String] the text value of the field
+  # @return [Array<String>] values which can be inserted into an XML document
+  def values_from_field(text)
+    text.split("\uFFFF", -1).map do |uncool|
+      uncool.gsub(/[\x00-\x08\x0B\x0C\x0E-\x1F\uFFFE\uFFFF]/, "\uFFFD")
+    end
+  end
 end
--- tidewater/spec/models/oai_item_spec.rb
+++ tidewater/spec/models/oai_item_spec.rb
@@ -1,36 +1,43 @@
 require "rails_helper"
 
 RSpec.describe OaiItem do
-  it 'does not allow most characters disallowed in XML' do
-    ["\x00", "\x12", "\uFFFE"].each do |badchar|
-      subject.title = "foo#{badchar}bar"
-      expect(subject.valid?).to be false
+  describe "metadata_for" do
+    it "replaces characters disallowed in XML with U+FFFD" do
+      ["\x00", "\x12", "\uFFFE"].each do |badchar|
+        subject.title = "foo#{badchar}bar"
+        expect(
+          subject.metadata_for("http://purl.org/dc/elements/1.1/title")
+        ).to eq ["foo\uFFFDbar"]
+      end
     end
-  end
 
-  it 'allows some special characters allowed in XML' do
-    ["\x09", "\x0A", "\x20", "\x7F", "\u0085"].each do |goodchar|
-      subject.title = "foo#{goodchar}bar"
-      expect(subject.valid?).to be true
+    it "allows some special characters allowed in XML" do
+      ["\x09", "\x0A", "\x20", "\x7F", "\u0085"].each do |goodchar|
+        subject.title = "foo#{goodchar}bar"
+        expect(
+          subject.metadata_for("http://purl.org/dc/elements/1.1/title")
+        ).to eq ["foo#{goodchar}bar"]
+      end
     end
-  end
 
-  it 'allows U+FFFF' do
-    subject.contributor = "Etaoin Shrdlu\uFFFFCmfwyp Vbgkqj"
-    expect(subject.valid?).to be true
-  end
+    it "returns fields with no value as an empty array" do
+      expect(
+        subject.metadata_for("http://purl.org/dc/elements/1.1/contributor")
+      ).to eq []
+    end
 
-  it 'returns fields with no value as an empty array' do
-    expect(subject.metadata_for 'http://purl.org/dc/elements/1.1/contributor').to eq []
-  end
+    it "returns fields with one value as an array with one element" do
+      subject.contributor = "Etaoin Shrdlu"
+      expect(
+        subject.metadata_for("http://purl.org/dc/elements/1.1/contributor")
+      ).to eq ["Etaoin Shrdlu"]
+    end
 
-  it 'returns fields with one value as an array with one element' do
-    subject.contributor = 'Etaoin Shrdlu'
-    expect(subject.metadata_for 'http://purl.org/dc/elements/1.1/contributor').to eq ['Etaoin Shrdlu']
-  end
-
-  it 'returns fields with multiple values as an array of values' do
-    subject.contributor = "Etaoin Shrdlu\uFFFFCmfwyp Vbgkqj"
-    expect(subject.metadata_for 'http://purl.org/dc/elements/1.1/contributor').to eq ['Etaoin Shrdlu', 'Cmfwyp Vbgkqj']
+    it "returns fields with multiple values as an array of values" do
+      subject.contributor = "Etaoin Shrdlu\uFFFFCmfwyp Vbgkqj"
+      expect(
+        subject.metadata_for("http://purl.org/dc/elements/1.1/contributor")
+      ).to eq ["Etaoin Shrdlu", "Cmfwyp Vbgkqj"]
+    end
   end
 end
