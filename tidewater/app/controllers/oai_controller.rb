a559a06c11c8171d8e5dabcff7ff065f6eb4f557
VivianChu <tchu@ucsd.edu>
2022-01-26 09:33:14 -0800

    Fix lint

--- tidewater/app/controllers/oai_controller.rb
+++ tidewater/app/controllers/oai_controller.rb
@@ -2,7 +2,7 @@ class OaiController < ApplicationController
   def index
     provider = OaiItemProvider.new
     response = provider.process_request(oai_params.to_h)
-    render :body => response, :content_type => 'text/xml'
+    render body: response, content_type: "text/xml"
   end
 
   private
--- tidewater/config/routes.rb
+++ tidewater/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  match 'oai', to: "oai#index", via: [:get, :post]
+  match "oai", to: "oai#index", via: [:get, :post]
 end
