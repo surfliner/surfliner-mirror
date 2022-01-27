24fad8ec3349c727144fb92faedf2c98ac0105e4
kibigo! <kibigo@ucsb.edu>
2022-01-27 13:49:43 -0800

    tidewater: drop OAI_NAMESPACE_SCHEME
    The ruby-oai gem presently requires a scheme of `oai:`, so this should
      not be configurable.

--- tidewater/app/providers/oai_item_provider.rb
+++ tidewater/app/providers/oai_item_provider.rb
@@ -1,7 +1,7 @@
 class OaiItemProvider < OAI::Provider::Base
   repository_name ENV.fetch("OAI_REPOSITORY_NAME")
   repository_url ENV.fetch("OAI_REPOSITORY_URL")
-  record_prefix "#{ENV.fetch("OAI_NAMESPACE_SCHEME")}:#{ENV.fetch("OAI_NAMESPACE_IDENTIFIER")}"
+  record_prefix "oai:#{ENV.fetch("OAI_NAMESPACE_IDENTIFIER")}"
   admin_email ENV.fetch("OAI_ADMIN_EMAIL")
   source_model OAI::Provider::ActiveRecordWrapper.new(OaiItem)
   sample_id ENV.fetch("OAI_SAMPLE_ID")
