fc8d7f0f471e4f85b69d3f0df413383957e0aa62
tamsin johnson <tomjohnson@ucsb.edu>
2022-01-27 16:04:24 -0800

    surfliner: add a generic `rails-entrypoint.sh`
    we're using several different, very similar rails entrypoint scripts. this is
    the one used by ~tidewater, and we're planning to reuse it with ~superskunk.

---
+++ scripts/rails-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f ${DOCKERROOT}/app/tmp/pids/server.pid
+
+# Then exec the container's main process
+# This is what's set as CMD in a) Dockerfile b) compose c) CI
+exec "$@"
