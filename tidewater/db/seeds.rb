860b7ba53d7484961dd79eb7274e726ec979ebbb
VivianChu <tchu@ucsd.edu>
2022-01-27 16:53:12 -0800

    Load sample data

--- tidewater/db/seeds.rb
+++ tidewater/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  item = OaiItem.create(title: "title 1#{i}", identifier: "ark://1234#{i}", creator: "surfliner #{i}")
+  puts "\n== loaded item ID - #{item.id}"
+end
