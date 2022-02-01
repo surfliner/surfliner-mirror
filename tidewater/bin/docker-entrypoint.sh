f0e5cbac9dc2f1e880167741561700f2f5e4b79a
VivianChu <tchu@ucsd.edu>
2022-02-01 09:25:48 -0800

    Tidewater: remove the previous entry point script

--- tidewater/bin/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /home/tidewater/app/tmp/pids/server.pid
-
-# Then exec the container's main process
-# This is what's set as CMD in a) Dockerfile b) compose c) CI
-exec "$@"
