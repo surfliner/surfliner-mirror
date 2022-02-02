847c4e846d8d332870591702c231bc0262ee7a1f
tamsin johnson <tomjohnson@ucsb.edu>
2022-02-01 18:21:28 -0800

    comet: a few more intializer debug statements

--- comet/config/initializers/redis_config.rb
+++ comet/config/initializers/redis_config.rb
@@ -3,3 +3,4 @@
 require "redis"
 config = YAML.safe_load(ERB.new(IO.read(Rails.root.join("config", "redis.yml"))).result)[Rails.env].with_indifferent_access
 Redis.current = Redis.new(config.merge(thread_safe: true))
+Rails.logger.debug { "Connected to Redis with #{Redis.current}" }
