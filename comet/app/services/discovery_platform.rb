864d4e6bc78aedda5447da5974aff15b3337b683
tamsin johnson <tomjohnson@ucsb.edu>
2022-02-08 09:52:09 -0800

    comet: document DiscoveryPlatform

--- comet/app/services/discovery_platform.rb
+++ comet/app/services/discovery_platform.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+##
+# Provides configuration for integration with discovery platforms.
+#
+# Comet offers systems for users to control visibility of resources in a variety
+# of discovery front-ends. This gives a single interface for options that
+# identify a given discovery platform throughout comet's internals and on its
+# message queues.
+#
+# These options include a unique `#name` for the platform (as a Symbol), the
+# message queue topics and routing keys relevant to the platform, and the
+# agent that identifies the platform in ACLs.
 DiscoveryPlatform = Struct.new(:name) do
   ##
   # @return [Hyrax::Group]
