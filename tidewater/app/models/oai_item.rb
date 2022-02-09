ad40c045da1739261455c9eb6c1e94eda973ce63
Longshou Situ <lsitu@ucsd.edu>
2022-02-08 18:57:51 -0800

    Convert JSON-LD content to OaiItem.

--- tidewater/app/models/oai_item.rb
+++ tidewater/app/models/oai_item.rb
@@ -92,4 +92,29 @@ class OaiItem < ApplicationRecord
     end
     result.doc.to_xml
   end
+
+  # Delimit multiple values field with U+FFFF and tagged value with U+FFFE.
+  #
+  # @param val [JSON] the value(s) of a field
+  # @return [string] delimited field value
+  def self.field_value_from_json(val)
+    if val.is_a?(Array)
+      val.map { |v| field_value_from_json(v) }.join("\uFFFF")
+    elsif val.is_a?(Hash)
+      "#{sanitize_value(val["@value"])}\uFFFE#{val["@language"]}"
+    else
+      sanitize_value(val)
+    end
+  end
+
+  # Convert JSON from Comet metadata API to OaiItem.
+  #
+  # @param json [JSON] the object metadata in JSON format
+  # @return [OaiItem] delimited field value
+  def self.from_json(resoure_uri, json)
+    oai_item = OaiItem.new(source_iri: resoure_uri)
+    JSON.parse(json).each { |k, v| oai_item.public_send("#{k}=", field_value_from_json(v)) if oai_item.respond_to?(k.to_s) }
+
+    oai_item
+  end
 end
--- tidewater/spec/models/oai_item_spec.rb
+++ tidewater/spec/models/oai_item_spec.rb
@@ -107,4 +107,33 @@ RSpec.describe OaiItem do
       expect(dc_metadata.elements["oai_dc:dc/dc:identifier"].text).to eq subject.identifier
     end
   end
+
+  describe "convert JSON-LD to OaiItem record" do
+    let(:resource_uri) { "http://superskunk.example.com/1234" }
+    let(:json_file) { Rails.root.join("spec", "fixtures", "mocked_metadata_response.json") }
+    let(:json_data) { File.open(json_file).read }
+    let(:oai_item) { OaiItem.from_json(resource_uri, json_data) }
+
+    let(:resource_uri) { "http://superskunk.example.com/1234" }
+    let(:title) { "Using Structural Metadata to Localize Experience of Digital Content\uFFFEen" }
+    let(:creator) { "Dushay, Naomi" }
+    let(:subject) { "Digital Libraries" }
+    let(:description) { "With the increasing technical sophistication of both information consumers and providers, there is increasing demand for more meaningful experiences of digital information. We present a framework that separates digital object experience, or rendering, from digital object storage and manipulation, so the rendering can be tailored to particular communities of users.\uFFFEen\uFFFFComment: 23 pages including 2 appendices, 8 figures" }
+    let(:date) { "2001-12-14" }
+    let(:type) { "e-print" }
+    let(:identifier) { "http://arXiv.org/abs/cs/0112017" }
+
+    it "builds OaiItem record" do
+      expect(oai_item).to be_a OaiItem
+
+      expect(oai_item.source_iri).to eq resource_uri
+      expect(oai_item.title).to eq title
+      expect(oai_item.creator).to eq creator
+      expect(oai_item.subject).to eq subject
+      expect(oai_item.description).to eq description
+      expect(oai_item.date).to eq date
+      expect(oai_item.type).to eq type
+      expect(oai_item.identifier).to eq identifier
+    end
+  end
 end
