ca04db43a6ab8e7a7ea8fb0ba370d88ee4d5df5d
Alexandra Dunn <dunn.alex@gmail.com>
2022-02-11 16:08:29 -0800

    superskunk: move all db touching into script

--- superskunk/scripts/db-migrate-seed.sh
+++ superskunk/scripts/db-migrate-seed.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 set -e
 
+PGPASSWORD=$DB_PASSWORD
+PGHOST=$DB_HOST
+PGUSER=$DB_USERNAME
+PGDATABASE=$DB_NAME
+export PGPASSWORD PGHOST PGUSER PGDATABASE
+
 db-wait.sh "$DB_HOST:$DB_PORT"
+
+echo "Ensuring database: $METADATA_DATABASE_NAME exists"
+psql -tc "SELECT 1 FROM pg_database WHERE datname = '$METADATA_DATABASE_NAME'" | grep -q 1 || createdb -e -w "$METADATA_DATABASE_NAME"
+
 bundle exec sequel -m ../db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
