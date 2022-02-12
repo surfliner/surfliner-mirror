cb0586053e71b46b4227a3789b64d0ba05edd0fd
Alexandra Dunn <dunn.alex@gmail.com>
2022-02-11 17:10:31 -0800

    superskunk: add healthz checks for rails, pg

---
+++ superskunk/config/initializers/okcomputer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Mount the health endpoint at /healthz
+# Review all health checks at /healthz/all
+OkComputer.mount_at = "healthz"
+
+OkComputer::Registry.deregister "database"
+
+DATABASE_URL = "postgresql://#{ENV["POSTGRES_USER"]}:#{ENV["POSTGRES_PASSWORD"]}@#{ENV["POSTGRES_HOST"]}:#{ENV["POSTGRES_PORT"]}/#{ENV["POSTGRES_DB"]}"
+OkComputer::Registry.register "sequel", OkComputer::SequelCheck.new(
+  database: Sequel.connect(DATABASE_URL),
+  migration_directory: "../db/sequel"
+)
