a3de405ec4d8ec9cd194ddf5173d4408bfcf4e0d
kibigo! <kibigo@ucsb.edu>
2022-02-14 10:24:14 -0800

    comet: configure to use surfliner_schema gem
    Adds a new `EnvSchemaReader` service to provide the underlying
    functionality of `EnvSchemaLoader`.

--- comet/app/services/env_schema_loader.rb
+++ comet/app/services/env_schema_loader.rb
@@ -2,19 +2,12 @@
 
 # this class overrides the standard schema loader to read schema names from the
 # environment, instead of taking an argument
-class EnvSchemaLoader < Hyrax::SimpleSchemaLoader
-  def metadata_models
-    @metadata_models ||= ENV["METADATA_MODELS"]
-      .to_s
-      .split(",")
-      .map(&:to_sym)
+class EnvSchemaLoader < SurflinerSchema::HyraxLoader
+  def initialize
+    super self.class.reader_class.instance
   end
 
-  def definitions(_schema_name)
-    metadata_models.map do |model|
-      schema_config(model)["attributes"].map do |name, config|
-        AttributeDefinition.new(name, config)
-      end
-    end.flatten
+  def self.reader_class
+    EnvSchemaReader
   end
 end
