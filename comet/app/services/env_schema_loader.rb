895d7953e6abd7f50b0aeb6dbb075b5d95451263
kibigo! <kibigo@ucsb.edu>
2022-03-11 10:17:19 -0800

    comet: Update for new surfliner_schema API

--- comet/app/services/env_schema_loader.rb
+++ comet/app/services/env_schema_loader.rb
@@ -3,11 +3,12 @@
 # this class overrides the standard schema loader to read schema names from the
 # environment, instead of taking an argument
 class EnvSchemaLoader < SurflinerSchema::HyraxLoader
-  def initialize
-    super self.class.reader_class.instance
-  end
-
-  def self.reader_class
-    EnvSchemaReader
+  def initialize(availability: :generic_object)
+    super(
+      *ENV["METADATA_MODELS"].to_s.split(",").map { |schema|
+        loader_class.instance.load(schema.to_sym)
+      },
+      availability: availability
+    )
   end
 end
