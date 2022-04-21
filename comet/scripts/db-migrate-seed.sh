f9c933297d77856fb4073e8538f978a44474fd20
Alexandra Dunn <dunn.alex@gmail.com>
2022-04-21 12:27:49 -0700

    comet: minimize prod docker image, add dev stage

--- comet/scripts/db-migrate-seed.sh
+++ comet/scripts/db-migrate-seed.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 set -e
 
-db-wait.sh "$DB_HOST:$DB_PORT"
+db_wait () {
+  host=$(printf "%s\n" "$1"| cut -d : -f 1)
+  port=$(printf "%s\n" "$1"| cut -d : -f 2)
+
+  shift 1
+
+  while ! nc -z "$host" "$port"
+  do
+    echo "waiting for $host:$port"
+    sleep 1
+  done
+
+  exec "$@"
+}
+
+db_wait "$DB_HOST:$DB_PORT"
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec sequel -m db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
 
-db-wait.sh "$SOLR_HOST:$SOLR_PORT"
+db_wait "$SOLR_HOST:$SOLR_PORT"
 
 bundle exec rails db:seed
