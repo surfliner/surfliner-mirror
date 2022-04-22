4104ba90759a24537f85d1a336d13849b4f0da64
Alexandra Dunn <dunn.alex@gmail.com>
2022-04-22 12:59:07 -0700

    scripts: use upstream versions of solrcloud scripts

--- scripts/solrcloud-assign-configset.sh
+++ scripts/solrcloud-assign-configset.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env sh
-
 COUNTER=0;
 
 if [ "$SOLR_ADMIN_USER" ]; then
   solr_user_settings="--user $SOLR_ADMIN_USER:$SOLR_ADMIN_PASSWORD"
 fi
 
-solr_config_name="${SOLR_CONFIG_NAME:-solrconfig}"
-solr_collection_name="${SOLR_CORE_NAME:-collection1}"
+solr_config_name="${SOLR_CONFIGSET_NAME:-solrconfig}"
+solr_collection_name="${SOLR_COLLECTION_NAME:-collection1}"
 
 # Solr Cloud Collection API URLs
 solr_collection_list_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=LIST"
-solr_collection_modify_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=MODIFYCOLLECTION&collection=$solr_collection_name&collection.configName=$solr_config_name"
+solr_collection_modify_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=MODIFYCOLLECTION&collection=${solr_collection_name}&collection.configName=${solr_config_name}"
 
 while [ $COUNTER -lt 30 ]; do
-  echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
   if nc -z "${SOLR_HOST}" "${SOLR_PORT}"; then
-    # shellcheck disable=SC2143,SC2086
-    echo $solr_collection_name
-    echo $solr_user_settings
-    echo $solr_collection_modify_url
-    if curl --silent $solr_user_settings "$solr_collection_list_url" | grep -q "${solr_collection_name}"; then
-      echo "-- Collection ${solr_collection_name} exists; setting ConfigSet ..."
+    if curl --silent $solr_user_settings "$solr_collection_list_url" | grep -q "$solr_collection_name"; then
+      echo "-- Collection ${solr_collection_name} exists; setting ${solr_config_name} ConfigSet ..."
+      echo $solr_collection_modify_url
       curl $solr_user_settings "$solr_collection_modify_url"
+      exit
+    else
+      echo "-- Collection ${solr_collection_name} does not exist; creating and setting ${solr_config_name} ConfigSet ..."
+      solr_collection_create_url="$SOLR_HOST:$SOLR_PORT/solr/admin/collections?action=CREATE&name=${solr_collection_name}&collection.configName=${solr_config_name}&numShards=1"
+      curl $solr_user_settings "$solr_collection_create_url"
       exit
     fi
   fi
+  echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
   COUNTER=$(( COUNTER+1 ));
   sleep 5s
 done
--- scripts/solrcloud-upload-configset.sh
+++ scripts/solrcloud-upload-configset.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
 COUNTER=0;
-# configdir example: /home/starlight/app/solr/config
+# /app/samvera/hyrax-webapp/solr/conf
 CONFDIR="${1}"
 
 if [ "$SOLR_ADMIN_USER" ]; then
   solr_user_settings="--user $SOLR_ADMIN_USER:$SOLR_ADMIN_PASSWORD"
 fi
 
-solr_config_name="${SOLR_CONFIG_NAME:-solrconfig}"
+solr_config_name="${SOLR_CONFIGSET_NAME:-solrconfig}"
 
 # Solr Cloud ConfigSet API URLs
 solr_config_list_url="http://$SOLR_HOST:$SOLR_PORT/api/cluster/configs?omitHeader=true"
-solr_config_upload_url="http://$SOLR_HOST:$SOLR_PORT/solr/admin/configs?action=UPLOAD&name=$solr_config_name"
+solr_config_upload_url="http://$SOLR_HOST:$SOLR_PORT/solr/admin/configs?action=UPLOAD&name=${solr_config_name}"
 
 while [ $COUNTER -lt 30 ]; do
   echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
@@ -22,13 +22,17 @@ while [ $COUNTER -lt 30 ]; do
       # the solr pods come up and report available before they are ready to accept trusted configs
       # only try to upload the config if auth is on.
       if curl --silent $solr_user_settings "$solr_config_list_url" | grep -q "$solr_config_name"; then
-      echo "-- ConfigSet already exists; skipping creation ...";
+        echo "-- ConfigSet already exists; skipping creation ...";
       else
         echo "-- ConfigSet for ${CONFDIR} does not exist; creating ..."
         # shellcheck disable=SC2035,SC2086
         (cd "$CONFDIR" && zip -r - *) | curl -X POST $solr_user_settings --header "Content-Type:application/octet-stream" --data-binary @- "$solr_config_upload_url"
       fi
       exit
+    else
+      echo "-- Solr at $solr_config_list_url is accepting unauthorized connections; we can't upload a trusted ConfigSet."
+      echo "--   It's possible SolrCloud is bootstrapping its configuration, so this process will retry."
+      echo "--   see: https://solr.apache.org/guide/8_6/configsets-api.html#configsets-upload"
     fi
   fi
   COUNTER=$(( COUNTER+1 ));
