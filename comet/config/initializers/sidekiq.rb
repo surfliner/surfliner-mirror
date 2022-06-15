a44ffaabac26f4787a70348eb4a7d66b5cb6a431
Matt Critchlow <mcritchlow@ucsd.edu>
2022-06-15 12:58:25 -0700

    comet: enabled readinessProbe for sidekiq
    Enable the upstream Hyrax chart `readinessProbe` for the worker/sidekiq
    `Deployment`, as suggested by the Sidekiq wiki [1].
    
    1. https://github.com/mperham/sidekiq/wiki/Kubernetes#health-checks

---
+++ comet/config/initializers/sidekiq.rb
@@ -0,0 +1,17 @@
+# config/initializers/sidekiq.rb
+SIDEKIQ_WILL_PROCESSES_JOBS_FILE = Rails.root.join("tmp/sidekiq_process_has_started_and_will_begin_processing_jobs").freeze
+
+Sidekiq.configure_server do |config|
+  # We touch and destroy files in the Sidekiq lifecycle to provide a
+  # signal to Kubernetes that we are ready to process jobs or not.
+  #
+  # Doing this gives us a better sense of when a process is actually
+  # alive and healthy, rather than just beginning the boot process.
+  config.on(:startup) do
+    FileUtils.touch(SIDEKIQ_WILL_PROCESSES_JOBS_FILE)
+  end
+
+  config.on(:shutdown) do
+    FileUtils.rm_f(SIDEKIQ_WILL_PROCESSES_JOBS_FILE)
+  end
+end
