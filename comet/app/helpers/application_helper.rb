64244b4ef19220eedc23c2cf8d27fc40c7b38e5e
tamsin johnson <tomjohnson@ucsb.edu>
2022-06-23 11:30:34 -0700

    comet: document ApplicationHelper#show_publish_button
    expand documentation to explain the status of this helper and its relationship
    to `Ability`.

--- comet/app/helpers/application_helper.rb
+++ comet/app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
   ##
-  # @param [Object] an object to check for publish ability
+  # given an ojbect, check whether the current user should see a publish
+  # button. this is a higher level of abstraction than the +Ability+ driven
+  # +#can?(:publish, obj)+ checks; use those if you just want to authorize
+  # the current user to publish.
   #
-  # @return [Boolean]
+  # @note this interface is intended to be polymorphic, but the current
+  # implementation is focused on +Hyrax::PcdmCollection+. if you use this
+  # check with other models, you probably need to implement the behavior
+  # you want.
+  #
+  # @param obj [Object] an object to check for publish ability
+  #
+  # @return [Boolean] true if the publish button should disply
   def show_publish_button?(obj)
     Rails.application.config.feature_collection_publish &&
       can?(:publish, obj)
