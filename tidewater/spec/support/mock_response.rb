95d359f5be35fe1dd1903bdb030532bbdd225e8d
kibigo! <kibigo@ucsb.edu>
2022-06-29 11:22:26 -0700

    tidewater: Just use `Accept:` not `Http_Accept:`
    The latter was implemented in some places due to confusion
    regarding how parts of (Rails? Rack? I forget) handle HTTP
    headers, but the former is what we actually want.

--- tidewater/spec/support/mock_response.rb
+++ tidewater/spec/support/mock_response.rb
@@ -4,8 +4,7 @@
 def mock_response(url)
   uri = URI(resource_uri)
   req = Net::HTTP::Get.new(uri)
-  req["Accept"] = "application/json"
-  req["HTTP_ACCEPT"] = "application/ld+json;profile=#{ENV.fetch("OAI_METADATA_PROFILE", "tag:surfliner.github.io,2022:api/oai_dc")}"
+  req["Accept"] = "application/ld+json;profile=#{ENV.fetch("OAI_METADATA_PROFILE", "tag:surfliner.github.io,2022:api/oai_dc")}"
 
   Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") { |http| http.request(req) }
 end
