07826c757e3492a7c8cda94cb6c81495ca0e1b8c
tamsin johnson <tomjohnson@ucsb.edu>
2022-07-06 09:47:18 -0700

    comet: fix file_metadata setup and storage adapter for download spec
    in the DownloadsController specs, use the correct FileMetadata setup (`#for`) is
    deprecated/non-sensical. rename the variable to reflect that this is metadata
    about the individual file, not the FileSet.
    
    use the real storage adapter to ensure the whole setup works with a real
    adapter.

--- comet/spec/controllers/hyrax/downloads_controller_spec.rb
+++ comet/spec/controllers/hyrax/downloads_controller_spec.rb
@@ -2,13 +2,16 @@
 
 require "rails_helper"
 
-RSpec.describe Hyrax::DownloadsController, storage_adapter: :memory, metadata_adapter: :test_adapter do
+RSpec.describe Hyrax::DownloadsController, metadata_adapter: :test_adapter do
   routes { Hyrax::Engine.routes }
   let(:user) { User.create(email: "moomin@example.com") }
-  let(:file_set_metadata) { Hyrax::FileMetadata.for(file: file) }
   let(:file_set) { Hyrax.persister.save(resource: Hyrax::FileSet.new) }
   let(:file) { Tempfile.new("moomin.jpg").tap { |f| f.write("def") } }
   let(:upload) { Hyrax.storage_adapter.upload(resource: file_set, file: file, original_filename: "Moomin.jpg") }
+
+  let(:file_metadata) do
+    Hyrax::FileMetadata.new(file_identifier: upload.id, alternate_identifier: upload.id)
+  end
 
   describe "#show" do
     it "returns unauthorized without a logged in user" do
@@ -27,15 +30,13 @@ RSpec.describe Hyrax::DownloadsController, storage_adapter: :memory, metadata_ad
       end
 
       it "sends the original file" do
-        allow(file).to receive(:original_filename).and_return("Moomin.jpg")
-        allow(file).to receive(:content_type).and_return("image/jpeg")
         file_set.file_ids << upload.id
         Hyrax.persister.save(resource: file_set)
-        file_set_metadata.file_identifier = upload.id
-        file_set_metadata.file_set_id = file_set.id
-        file_set_metadata.size = upload.size
-        file_set_metadata.id = upload.id
-        Hyrax.persister.save(resource: file_set_metadata)
+        file_metadata.file_identifier = upload.id
+        file_metadata.file_set_id = file_set.id
+        file_metadata.size = upload.size
+        file_metadata.id = upload.id
+        Hyrax.persister.save(resource: file_metadata)
         get :show, params: {id: file_set.id}
         file.rewind
         expect(response.body).to eq file.read
