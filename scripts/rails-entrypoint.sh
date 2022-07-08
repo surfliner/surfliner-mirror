2b937f53d14a05b572a87a61536c37081ac0b43c
Kristian De Castro <kddecastro@ucsd.edu>
2022-07-08 12:03:17 -0700

    Add vault secret compatibilty for superskunk staging

--- scripts/rails-entrypoint.sh
+++ scripts/rails-entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f ${DOCKERROOT}/app/tmp/pids/server.pid
 
+# source env vars from vault secret file
+if [ "$VAULT_FILE" ] && [ -e "$VAULT_FILE" ]; then
+  . "$VAULT_FILE"
+fi
+
 # Then exec the container's main process
 # This is what's set as CMD in a) Dockerfile b) compose c) CI
 exec "$@"
