9385c786f095eb09a22360ece407e93c74cf7f36
kibigo! <kibigo@ucsb.edu>
2022-07-18 16:26:15 -0700

    tidewater: Set User-Agent
    Specifiable through the environment, but defaults to
    `surfliner.tidewater`.

--- tidewater/spec/support/mock_response.rb
+++ tidewater/spec/support/mock_response.rb
@@ -5,6 +5,7 @@ def mock_response(url)
   uri = URI(resource_uri)
   req = Net::HTTP::Get.new(uri)
   req["Accept"] = "application/ld+json;profile=\"#{ENV.fetch("OAI_METADATA_PROFILE", "tag:surfliner.gitlab.io,2022:api/oai_dc")}\""
+  req["User-Agent"] = ENV.fetch("USER_AGENT_PRODUCT_NAME", "surfliner.tidewater")
 
   Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") { |http| http.request(req) }
 end
