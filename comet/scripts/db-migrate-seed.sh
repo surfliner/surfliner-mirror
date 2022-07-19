3e1869b724f0b804a116c72abe2c9f7b6fc96da4
Matt Critchlow <mcritchlow@ucsd.edu>
2022-07-19 13:22:41 -0700

    comet: teach db-migrate-seed to use db-setup
    This allows more flexibility in the database setup, as it exposes the
    `DATABASE_COMMAND` environment variable for ~comet.

--- comet/scripts/db-migrate-seed.sh
+++ comet/scripts/db-migrate-seed.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 set -e
 
-db_wait () {
-  host=$(printf "%s\n" "$1"| cut -d : -f 1)
-  port=$(printf "%s\n" "$1"| cut -d : -f 2)
+# shellcheck source=/dev/null
+. db-setup.sh
 
-  shift 1
-
-  while ! nc -z "$host" "$port"
-  do
-    echo "waiting for $host:$port"
-    sleep 1
-  done
-
-  exec "$@"
-}
-
-db_wait "$DB_HOST:$DB_PORT"
-bundle exec rails db:create
-bundle exec rails db:migrate
 bundle exec sequel -m db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
 
-db_wait "$SOLR_HOST:$SOLR_PORT"
+db-wait.sh "$SOLR_HOST:$SOLR_PORT"
 
 bundle exec rails db:seed
