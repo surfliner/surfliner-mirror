f9f1210e718b8937e165f6d7c8ac48d0323fb124
tamsin johnson <tomjohnson@ucsb.edu>
2022-08-09 13:34:19 -0700

    comet: fix creation of admin sets
    setup the `admin_set_model` in configuration.

--- comet/config/initializers/hyrax.rb
+++ comet/config/initializers/hyrax.rb
@@ -5,6 +5,8 @@ Hyrax.config do |config|
   config.register_curation_concern :generic_object
   config.disable_wings = true
 
+  config.admin_set_model = "Hyrax::AdministrativeSet"
+
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
