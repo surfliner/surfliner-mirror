854cda0a0effb09fd3d38deb646fb3108b022c86
tamsin johnson <tomjohnson@ucsb.edu>
2022-08-15 10:19:34 -0700

    superskunk: don't lookup metadata adapter for every request
    the metadata adapter can be looked up once per server process, not once per
    request.
    
    this change also has the advantage of providing a hook for overriding
    configuration for specific tests.

--- superskunk/config/initializers/superskunk.rb
+++ superskunk/config/initializers/superskunk.rb
@@ -1,5 +1,9 @@
 module Superskunk
   def self.comet_query_service
-    Valkyrie::MetadataAdapter.find(:comet_metadata_store).query_service
+    metadata_adapter.query_service
+  end
+
+  def self.metadata_adapter
+    @metadata_adapter ||= Valkyrie::MetadataAdapter.find(:comet_metadata_store)
   end
 end
