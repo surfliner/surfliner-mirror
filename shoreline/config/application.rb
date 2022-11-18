b3665081a91e52af872409a5456ffc09cbd36fe7
Alexandra Dunn <dunn.alex@gmail.com>
2022-11-18 10:14:59 -0800

    shoreline: run app:update for rails 6

--- shoreline/config/application.rb
+++ shoreline/config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module Shoreline
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     if ENV["RAILS_LOG_TO_STDOUT"].present?
       logger = ActiveSupport::Logger.new($stdout)
