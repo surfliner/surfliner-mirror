9ac61572352b22b139d3da5ba0e6a00759a99587
Alexandra Dunn <dunn.alex@gmail.com>
2022-11-30 13:12:58 -0800

    solrcloud-upload-configset: upload even when a configset exists
    When the application's solr config is updated, we need to add it to Solr.

--- scripts/solrcloud-upload-configset.sh
+++ scripts/solrcloud-upload-configset.sh
@@ -17,17 +17,14 @@ solr_config_upload_url="http://$SOLR_HOST:$SOLR_PORT/solr/admin/configs?action=U
 while [ $COUNTER -lt 30 ]; do
   echo "-- Looking for Solr (${SOLR_HOST}:${SOLR_PORT})..."
   if nc -z "${SOLR_HOST}" "${SOLR_PORT}"; then
+    # the solr pods come up and report available before they are ready to accept trusted configs
+    # only try to upload the config if auth is on.
+    #
     # shellcheck disable=SC2143,SC2086
     if curl --silent --user 'fake:fake' "$solr_config_list_url" | grep -q '401'; then
-      # the solr pods come up and report available before they are ready to accept trusted configs
-      # only try to upload the config if auth is on.
-      if curl --silent $solr_user_settings "$solr_config_list_url" | grep -q "$solr_config_name"; then
-        echo "-- ConfigSet already exists; skipping creation ...";
-      else
-        echo "-- ConfigSet for ${CONFDIR} does not exist; creating ..."
-        # shellcheck disable=SC2035,SC2086
-        (cd "$CONFDIR" && zip -r - *) | curl -X POST $solr_user_settings --header "Content-Type:application/octet-stream" --data-binary @- "$solr_config_upload_url"
-      fi
+      echo "-- Uploading ConfigSet for ${CONFDIR} ..."
+      # shellcheck disable=SC2035,SC2086
+      (cd "$CONFDIR" && zip -r - *) | curl -X POST $solr_user_settings --header "Content-Type:application/octet-stream" --data-binary @- "$solr_config_upload_url"
       exit
     else
       echo "-- Solr at $solr_config_list_url is accepting unauthorized connections; we can't upload a trusted ConfigSet."
