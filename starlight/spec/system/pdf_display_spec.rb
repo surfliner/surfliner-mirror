5a9956a32378999884ca8e33764b1536e39a51f9
Matt Critchlow <mcritchlow@ucsd.edu>
2023-01-13 10:57:27 -0800

    starlight: fix pdf_display_spec
    The previous method chain we were calling to create an exhibit and
    upload a resource was resulting in test errors that were not present by
    performing the same action in the UI. So we're moving to have the test
    use the UI instead

--- starlight/spec/system/pdf_display_spec.rb
+++ starlight/spec/system/pdf_display_spec.rb
@@ -3,33 +3,38 @@
 require "rails_helper"
 
 RSpec.describe "Display embedded PDFs", :clean, type: :system, js: true do
-  let!(:exhibit) do
-    Spotlight::Exhibit.create(title: "test")
-  end
-
-  let!(:pdf) do
-    resource = Spotlight::Resources::Upload.new(
-      data: {spotlight_upload_title_tesim: "pdf"},
-      exhibit: exhibit
-    )
-
-    image = Spotlight::FeaturedImage.new
-    image.image.store!(File.open(File.join(fixture_path, "blank.pdf")))
-    resource.upload = image
-    resource.save_and_index
-    resource
-  end
-
   let(:site_admin) { FactoryBot.create(:omniauth_site_admin) }
 
   before do
     omniauth_setup_dev_auth_for(site_admin)
     sign_in
+    allow(Spotlight::DefaultThumbnailJob).to receive(:perform_later)
+    allow_any_instance_of(CarrierWave::Downloader::Base).to receive(:skip_ssrf_protection?).and_return(true)
+    visit "/"
+    click_link site_admin.user_key
+    click_link "Create new exhibit"
+    fill_in("Title", with: "Test Exhibit")
+    fill_in("Tag list", with: "testing")
+    click_button "Create Exhibit"
+    visit "/starlight/test-exhibit/edit"
+    page.check("exhibit_published")
+    click_button "Save changes"
+
+    visit("/starlight/test-exhibit/resources/new")
+    click_link "Upload item"
+    page.attach_file("resources_upload[url]", File.join(fixture_path, "blank.pdf"))
+    fill_in("Title", with: "PDF")
+    within "#new_resources_upload" do
+      click_button "Add item"
+    end
   end
 
   context "when viewing a PDF" do
     it "uses a direct embed" do
-      visit "/starlight/test/catalog/#{exhibit.id}-#{pdf.id}"
+      visit "/starlight/test-exhibit/catalog?utf8=%E2%9C%93&exhibit_id=test-exhibit&search_field=all_fields&q="
+      expect(page).to have_content "PDF"
+
+      click_link "PDF"
 
       expect(page).to have_selector "embed"
       expect(page).not_to have_selector ".universal-viewer-iframe"
