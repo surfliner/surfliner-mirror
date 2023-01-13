819e133565d3a25d9ef854d7c3e2d87f07293029
Alexandra Dunn <dunn.alex@gmail.com>
2023-01-13 12:52:07 -0800

    starlight: update capybara options to match comet

--- starlight/spec/support/capybara_selenium.rb
+++ starlight/spec/support/capybara_selenium.rb
@@ -3,8 +3,7 @@
 require "capybara/rspec"
 require "selenium-webdriver"
 
-Capybara.server = :puma
-Capybara.default_max_wait_time = 20
+DOWNLOAD_PATH = Rails.root.join("tmp/Downloads")
 
 Capybara.register_driver :selenium_chrome_starlight do |app|
   browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
@@ -12,13 +11,23 @@ Capybara.register_driver :selenium_chrome_starlight do |app|
     opts.add_argument("--disable-gpu") if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument("--disable-site-isolation-trials")
+    opts.add_argument("--window-size=1440,1440")
+    opts.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+    opts.add_argument("--disable-features=VizDisplayCompositor")
   end
+
+  # Different versions of Chrome/selenium-webdriver require setting differently - just set them all
+  browser_options.add_preference("download.default_directory", DOWNLOAD_PATH)
+  browser_options.add_preference(:download, prompt_for_download: false, default_directory: DOWNLOAD_PATH)
+  browser_options.add_preference(:browser, set_download_behavior: {behavior: "allow"})
 
   Capybara::Selenium::Driver.new(app, **{browser: :remote, url: ENV["SELENIUM_URL"], capabilities: browser_options})
 end
 
+Capybara.server = :puma
 Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-Capybara.default_max_wait_time = ENV.fetch("CAPYBARA_WAIT_TIME", 10) # We may have a slow application, let's give it some time.
+# We may have a slow application, let's give it some time.
+Capybara.default_max_wait_time = ENV.fetch("CAPYBARA_WAIT_TIME", 30).to_i
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
