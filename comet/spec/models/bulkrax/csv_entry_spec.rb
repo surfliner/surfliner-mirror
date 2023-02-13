b58204dbedd1043cde7f044e5036b30e79ae5f4e
tamsin johnson <tomjohnson@ucsb.edu>
2023-02-13 11:54:35 -0800

    comet: minimize stubbing in bulkrax/csv_entry_spec

--- comet/spec/models/bulkrax/csv_entry_spec.rb
+++ comet/spec/models/bulkrax/csv_entry_spec.rb
@@ -4,12 +4,19 @@ require "rails_helper"
 
 RSpec.describe Bulkrax::CsvEntry do
   describe "Export metadata" do
-    subject(:csv_entry) { described_class.new(importerexporter: exporter) }
+    subject(:csv_entry) do
+      described_class.new(importerexporter: exporter, identifier: work.id)
+    end
 
     let(:user) { User.find_or_create_by(email: "comet-admin@library.ucsb.edu") }
 
     context "with object fields and no prefix" do
-      let(:work_obj) { GenericObject.new(title: "Test Object Title", title_alternative: "An Alternative Title", creator: "Tester") }
+      let(:work) do
+        w = GenericObject.new(title: "Test Object Title",
+                          title_alternative: "An Alternative Title",
+                          creator: "Tester")
+        Hyrax.persister.save(resource: w)
+      end
 
       let(:exporter) do
         Bulkrax::Exporter.new(name: "Export from Importer",
@@ -21,14 +28,6 @@ RSpec.describe Bulkrax::CsvEntry do
                               limit: 0,
                               field_mapping: {},
                               generated_metadata: false)
-      end
-
-      before do
-        allow(csv_entry).to receive(:hyrax_record).and_return(work_obj)
-        allow(work_obj).to receive(:id).and_return("test_object")
-        allow(work_obj).to receive(:member_of_work_ids).and_return([])
-        allow(work_obj).to receive(:in_work_ids).and_return([])
-        allow(work_obj).to receive(:member_work_ids).and_return([])
       end
 
       it "populates metadata" do
