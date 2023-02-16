09778d42f5d4cd0efb534f0a0ca61f1345d6bfc4
tamsin johnson <tomjohnson@ucsb.edu>
2023-02-16 14:56:35 -0800

    comet: reduce browser interactions in add_object_to_collection_spec
    these tests relied on browser interactions extensively for setup. since these
    can be complicated and often give rise to intermittent/docker service errors,
    especially in the CI environment, it's a good idea to use other tools to setup
    the test. use the browser only for the parts of the spec that are intended to
    test the full stack.

--- comet/spec/system/add_object_to_collection_spec.rb
+++ comet/spec/system/add_object_to_collection_spec.rb
@@ -2,11 +2,27 @@
 
 require "rails_helper"
 
-RSpec.describe "Generic Objects", type: :system, js: true do
-  let(:user) { User.find_or_create_by(email: "comet-admin@library.ucsb.edu") }
-  before { sign_in user }
+RSpec.describe "Objects", type: :system, js: true do
+  let(:collection) do
+    col = Hyrax::PcdmCollection.new(title: ["Test Collection for Object Assignment"],
+      collection_type_gid: collection_type.to_global_id.to_s)
+    col = Hyrax.persister.save(resource: col)
+    col
+  end
 
-  context "during object creation" do
+  let(:collection_type) do
+    type = Hyrax::CollectionType.create(title: "Spec Type")
+    Hyrax::CollectionTypeParticipant.create(hyrax_collection_type: type, agent_id: user.id, agent_type: "user", access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS)
+    type
+  end
+  let(:user) { User.find_or_create_by(email: "comet-admin@library.ucsb.edu") }
+
+  before do
+    collection # create the collection explictly
+    sign_in user
+  end
+
+  context "from the new object form" do
     it "can create a new object and assign a Collection to it" do
       visit "/concern/generic_objects/new"
 
@@ -18,114 +34,50 @@ RSpec.describe "Generic Objects", type: :system, js: true do
     end
   end
 
-  context "after object creation" do
-    let!(:collection_type) { Hyrax::CollectionType.create(title: "Spec Type") }
+  context "from the object show page" do
+    let(:object) do
+      obj = GenericObject.new(title: "Object Added to Collection by Spec",
+        visibility: "open",
+        edit_users: [user])
+      obj = Hyrax.persister.save(resource: obj)
+      Hyrax.index_adapter.save(resource: obj)
+      obj
+    end
 
     it "can assign a Collection to it" do
-      visit "/dashboard"
-      click_on "Collections"
-      find("#add-new-collection-button").click
-
-      within("div#collectiontypes-to-create") do
-        choose("Spec Type")
-        click_on("Create collection")
-      end
-
-      fill_in("Title", with: "Test Collection 3")
-
-      click_on("Save")
-      persisted_colls = Hyrax.query_service.find_all_of_model(model: Hyrax::PcdmCollection)
-      persisted_coll = persisted_colls.find do |col|
-        col.title == ["Test Collection 3"]
-      end
-
-      visit "/concern/generic_objects/new"
-      fill_in("Title", with: "My Title 2")
-      choose("generic_object_visibility_open")
-
-      # ensure that the form fields are fully populated
-      sleep(1.seconds)
-      click_on "Save"
+      visit main_app.hyrax_generic_object_path(id: object.id, locale: "en")
 
       click_button "Add to collection"
-      select_member_of_collection(persisted_coll)
+      select_member_of_collection(collection)
       click_button "Save changes"
 
-      objs = Hyrax.query_service.find_all_of_model(model: GenericObject)
-      persisted_obj = objs.find do |gob|
-        gob.title == ["My Title 2"]
-      end
-      expect(persisted_obj.member_of_collection_ids).to eq([persisted_coll.id])
+      expect(object.member_of_collection_ids).to contain_exactly collection.id
 
-      visit "/dashboard"
-      click_on "Collections"
-      click_on("Display all details of Test Collection 3")
+      visit "/dashboard/collections/#{collection.id}"
 
-      expect(page).to have_content("Test Collection 3")
-      expect(page).to have_content("My Title 2")
+      expect(page).to have_content("Test Collection for Object Assignment")
+      expect(page).to have_content("Object Added to Collection by Spec")
     end
 
     it "can assign multiple Collections to it" do
-      visit "/dashboard"
-      click_on "Collections"
+      col = Hyrax::PcdmCollection.new(title: ["Another Test Collection for Object Assignment"],
+        collection_type_gid: collection_type.to_global_id.to_s)
+      other_collection = Hyrax.persister.save(resource: col)
 
-      find("#add-new-collection-button").click
-      within("div#collectiontypes-to-create") do
-        choose("Spec Type")
-        click_on("Create collection")
-      end
-
-      fill_in("Title", with: "Test Collection 1")
-
-      click_on("Save")
-
-      visit "/dashboard"
-      click_on "Collections"
-
-      find("#add-new-collection-button").click
-      within("div#collectiontypes-to-create") do
-        choose("Spec Type")
-        click_on("Create collection")
-      end
-
-      fill_in("Title", with: "Test Collection 2")
-
-      click_on("Save")
-
-      expect(page).to have_content("Test Collection 2")
-
-      persisted_collections = Hyrax.query_service.find_all_of_model(model: Hyrax::PcdmCollection)
-      persisted_collection1 = persisted_collections.find do |col|
-        col.title == ["Test Collection 1"]
-      end
-      persisted_collection2 = persisted_collections.find do |col|
-        col.title == ["Test Collection 2"]
-      end
-
-      visit "/concern/generic_objects/new"
-      fill_in("Title", with: "My Title")
-      choose("generic_object_visibility_open")
-
-      # ensure that the form fields are fully populated
-      sleep(1.seconds)
-      click_on "Save"
+      visit main_app.hyrax_generic_object_path(id: object.id, locale: "en")
 
       click_button "Add to collection"
-      select_member_of_collection(persisted_collection1)
+      select_member_of_collection(collection)
       click_button "Save changes"
 
-      gobjs = Hyrax.query_service.find_all_of_model(model: GenericObject)
-      persisted_object = gobjs.find do |gob|
-        gob.title == ["My Title"]
-      end
+      visit main_app.hyrax_generic_object_path(id: object.id, locale: "en")
 
-      visit main_app.hyrax_generic_object_path(id: persisted_object.id.to_s,
-        locale: "en")
       click_button "Add to collection"
-      select_member_of_collection(persisted_collection2)
+      select_member_of_collection(other_collection)
       click_button "Save changes"
 
-      expect(persisted_object.member_of_collection_ids).to contain_exactly(persisted_collection1.id, persisted_collection2.id)
+      expect(persisted_object.member_of_collection_ids)
+        .to contain_exactly(collection.id, other_collection.id)
     end
   end
 end
