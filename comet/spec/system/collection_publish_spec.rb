8ef7717f406c130b7e8e9d093b686c3130ab2cdf
tamsin johnson <tomjohnson@ucsb.edu>
2023-02-21 16:21:21 -0800

    comet: refactor more collection specs to use the factory

--- comet/spec/system/collection_publish_spec.rb
+++ comet/spec/system/collection_publish_spec.rb
@@ -5,13 +5,7 @@ require "rails_helper"
 RSpec.describe "Publish Collection", type: :system, js: true do
   let(:user) { User.find_or_create_by(email: "comet-admin@library.ucsb.edu") }
 
-  let(:collection_type) { Hyrax::CollectionType.create(title: "Test Type") }
-  let(:collection_type_gid) { collection_type.to_global_id.to_s }
-
-  let(:collection) do
-    col = Hyrax::PcdmCollection.new(title: ["Test Collection"], collection_type_gid: collection_type_gid)
-    Hyrax.persister.save(resource: col)
-  end
+  let(:collection) { FactoryBot.valkyrie_create(:collection, :with_permission_template, user: user) }
 
   let(:object) do
     obj = ::GenericObject.new(title: ["Test Object"], member_of_collection_ids: [collection.id])
@@ -20,13 +14,6 @@ RSpec.describe "Publish Collection", type: :system, js: true do
 
   before {
     sign_in(user)
-
-    Hyrax::Collections::PermissionsCreateService.create_default(collection: collection, creating_user: user)
-    collection.permission_manager.read_users += [user.user_key]
-    collection.permission_manager.edit_users += [user.user_key]
-    collection.permission_manager.acl.save
-
-    Hyrax.index_adapter.save(resource: collection)
     Hyrax.index_adapter.save(resource: object)
   }
 
