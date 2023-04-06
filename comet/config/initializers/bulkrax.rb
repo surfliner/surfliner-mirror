b93ab1de8a7f55bec0e531220bdff83a81fde932
Alexandra Dunn <dunn.alex@gmail.com>
2023-04-06 14:43:21 -0700

    comet: turn off visibility selector in bulkrax UI

--- comet/config/initializers/bulkrax.rb
+++ comet/config/initializers/bulkrax.rb
@@ -10,6 +10,14 @@ Bulkrax.setup do |config|
   config.default_work_type = "GenericObject"
 end
 
+Bulkrax.parsers = [
+  {name: "OAI - Dublin Core", class_name: "Bulkrax::OaiDcParser", partial: "oai_fields"},
+  {name: "OAI - Qualified Dublin Core", class_name: "Bulkrax::OaiQualifiedDcParser", partial: "oai_fields"},
+  {name: "CSV - Comma Separated Values", class_name: "Bulkrax::CsvParser", partial: "csv_fields_no_visibility"},
+  {name: "Bagit", class_name: "Bulkrax::BagitParser", partial: "bagit_fields"},
+  {name: "XML", class_name: "Bulkrax::XmlParser", partial: "xml_fields"}
+]
+
 module HasMappingExt
   ##
   # Field of the model that can be supported
