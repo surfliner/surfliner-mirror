ea5224fcf2838b1021a2ed69c10e51ac149091cb
Longshou Situ <lsitu@ucsd.edu>
2023-04-13 15:03:56 -0700

    comet: fix paging issue and tab content display issue.

--- comet/app/assets/javascripts/tabs_paging.js
+++ comet/app/assets/javascripts/tabs_paging.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-  // open tab on click
-  $('ul#items-tabs > li > a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-
-    // getting tab name
-    var tabId = $(this).attr("href").substr(1);
-
-    window.location.href = buildPagingUrl(window.location.href, tabId);
-  });
-
   // store the current tab
   $("ul#items-tabs > li > a").on("shown.bs.tab", function (e) {
     var tabId = $(this).attr("href").substr(1);
@@ -21,12 +10,28 @@ $(document).ready(function(){
   var tabHash = window.location.hash || '#file_sets';
   $('ul#items-tabs > li > a[href="' + tabHash + '"]').tab('show');
 
-  // change hash value for all tabs
-  $('ul.pagination > li > a').each(function(i, a) {
-    a.href = buildPagingUrl(a.href, tabHash.substr(1));
+  // load full page when page link is clicked instead.
+  tabId = tabHash.substr(1);
+  $(`${tabHash} ul.pagination > li > a`).each(function(i, a) {
+    var url = buildPagingUrl(a.href, tabId);
+
+    $(a).removeAttr('href');
+    $(a).css('cursor', 'pointer');
+    $(a).click(function (e) {
+      e.preventDefault();
+      window.location.href = url;
+    });
   });
 });
 
+// load the page for the tab
+function load_tab_content(tab) {
+  $(`#${tab}`).tab('show');
+
+  window.location.href = buildPagingUrl(window.location.href, tab);
+}
+  
+// rebuild the url to include the tab id to load file sets or component data
 function buildPagingUrl(url, tabId) {
   var urlObj = new URL(url);
 
