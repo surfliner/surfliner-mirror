b94915a5f61a5ac2c8c90cd80f5b0fe849897e52
kibigo! <kibigo@ucsb.edu>
2023-04-17 10:31:49 -0700

    comet: Support non‐SQL adapters in custom queries
    The custom queries for finding FileMetadata resources only worked with
    the `Valkyrie::Sequel` adapter, meaning they failed in specs which use
    the memory adapter.

--- comet/app/services/custom_queries/find_file_metadata.rb
+++ comet/app/services/custom_queries/find_file_metadata.rb
@@ -24,13 +24,7 @@ module CustomQueries
       return [] unless file_set.try(:file_ids)
       return enum_for(:find_files, file_set: file_set).to_a unless block_given?
 
-      file_set.file_ids.each do |file_id|
-        query_json = {file_identifier: [id: file_id.to_s]}.to_json
-        refs = query_service.run_query(query_service.send(:find_inverse_references_with_model_query),
-          query_json,
-          "Hyrax::FileMetadata")
-        refs.each { |fm| yield fm }
-      end
+      files_for_file_set(file_set) { |fm| yield fm }
     end
 
     # Find original file id of a given file set resource, and map to file metadata resource
@@ -68,18 +62,38 @@ module CustomQueries
       return enum_for(:find_many_file_metadata_by_use, resource: resource, use: use) unless
         block_given?
 
-      # TODO: replace this with an efficient query that joins on the resource.
-      resource.file_ids.each do |file_id|
-        query_json = {file_identifier: [id: file_id.to_s]}.to_json
-        refs = query_service.run_query(query_service.send(:find_inverse_references_with_model_query),
-          query_json,
-          "Hyrax::FileMetadata")
-
-        refs.each { |fm| yield fm if fm.type.include?(use) }
-      end
+      files_for_file_set(resource) { |fm| yield fm if fm.type.include?(use) }
     end
 
     private
+
+    ##
+    # Yields the +FileMetadata+ objects corresponding to the provided +FileSet+.
+    #
+    # TODO: replace this with an efficient query that joins on the resource.
+    def files_for_file_set(file_set)
+      if query_service.is_a?(Valkyrie::Sequel::MetadataAdapter)
+        file_set.file_ids.each do |file_id|
+          query_json = {file_identifier: [id: file_id.to_s]}.to_json
+          # This relies on a private method in the +Valkyrie::Sequel+ query
+          # adapter.
+          query_service.run_query(
+            query_service.send(:find_inverse_references_with_model_query),
+            query_json,
+            "Hyrax::FileMetadata"
+          ).each { |fm| yield fm }
+        end
+      else
+        file_set.file_ids.each do |file_id|
+          # A slower implementation for non‐SQL scenarios.
+          query_service.find_inverse_references_by(
+            id: file_id.to_s,
+            property: :file_identifier,
+            model: Hyrax::FileMetadata
+          ).each { |fm| yield fm }
+        end
+      end
+    end
 
     ##
     # this doesn't quite work. :(
