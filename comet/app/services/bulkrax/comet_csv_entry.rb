4ae677e48489b0d57b95186f108dd6674e9165f0
Longshou Situ <lsitu@ucsd.edu>
2023-05-15 08:42:18 -0700

    comet: add back build_m3_metadata with comment for potential need.

--- comet/app/services/bulkrax/comet_csv_entry.rb
+++ comet/app/services/bulkrax/comet_csv_entry.rb
@@ -30,6 +30,10 @@ module Bulkrax
       mapping[related_parents_parsed_mapping] = mapping[related_parents_parsed_mapping] || {}
       build_relationship_metadata
 
+      # Extract M3 properties before other configured mappings.
+      # Note: This is not needed unless an empty or incomplete mapping fields are configured.
+      # build_m3_metadata
+
       build_mapping_metadata
 
       save!
@@ -44,6 +48,37 @@ module Bulkrax
       file_sets = hyrax_record.file_set? ? Array.wrap(hyrax_record) : Hyrax.custom_queries.find_child_file_sets(resource: hyrax_record)
       filenames = map_file_sets(file_sets)
       handle_join_on_export(file_mapping, filenames, mapping["file"]&.[]("join")&.present?)
+    end
+
+    ##
+    # Override to remove the properties that have custom mappings from parsed_metadata, which are extracted from M3 mappings.
+    def build_value(key, value)
+      data = hyrax_record.send(key.to_s)
+      if data.is_a?(ActiveTriples::Relation)
+        if value["join"]
+          parsed_metadata[key_for_export(key)] = data.map { |d| prepare_export_data(d) }.join(Bulkrax.multi_value_element_join_on).to_s
+        else
+          data.each_with_index do |d, i|
+            parsed_metadata["#{key_for_export(key)}_#{i + 1}"] = prepare_export_data(d)
+          end
+        end
+      else
+        # Now delete the mapping from m3 since custom mapping is defined for the property
+        parsed_metadata.delete(key) if parsed_metadata.key? key
+
+        parsed_metadata[key_for_export(key)] = prepare_export_data(data)
+      end
+    end
+
+    ##
+    # Add mappings from M3
+    def build_m3_metadata
+      Bulkrax::ValkyrieObjectFactory.schema_properties(hyrax_record.class).each do |pro|
+        value = hyrax_record.send(pro)
+        parsed_metadata[pro] = value.present? ? value.join(Bulkrax.multi_value_element_join_on) : nil
+      end
+
+      parsed_metadata["title"] = hyrax_record.send(:title).first
     end
 
     ##
