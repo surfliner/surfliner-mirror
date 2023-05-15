4b8bc86c0c1f50b03e76228f617b198f5bf05659
Alexandra Dunn <dunn.alex@gmail.com>
2023-05-15 22:01:33 +0000

    db-migrate-seed: pass pg url explicitly

--- superskunk/scripts/db-migrate-seed.sh
+++ superskunk/scripts/db-migrate-seed.sh
@@ -4,6 +4,10 @@ set -e
 db-wait.sh "$DB_HOST:$DB_PORT"
 
 echo "Ensuring database: $METADATA_DATABASE_NAME exists"
-psql -tc "SELECT 1 FROM pg_database WHERE datname = '$METADATA_DATABASE_NAME'" | grep -q 1 || createdb -e -w "$METADATA_DATABASE_NAME"
+psql -tc "SELECT 1 FROM pg_database WHERE datname = '$METADATA_DATABASE_NAME'" \
+     postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME" | \
+  grep -q 1 || \
+  createdb -e -w "$METADATA_DATABASE_NAME" \
+           postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
 
 bundle exec sequel -m ../db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
