13a9d6204b7d8721d620ad0e8615cdb7dbf6bbdb
Alexandra Dunn <dunn.alex@gmail.com>
2023-05-22 13:13:51 -0700

    superskunk: use db in url when checking for db?

--- superskunk/scripts/db-migrate-seed.sh
+++ superskunk/scripts/db-migrate-seed.sh
@@ -7,8 +7,8 @@ psql_base="postgres://${POSTGRESQL_USERNAME}:${POSTGRESQL_PASSWORD}@${POSTGRESQL
 database_url="${psql_base}/${POSTGRESQL_DATABASE}"
 
 echo "Ensuring database: $POSTGRESQL_DATABASE exists"
-psql -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRESQL_DATABASE'" "$psql_base" | \
+psql -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRESQL_DATABASE'" "$database_url" | \
   grep -q 1 || \
-  psql -tc "CREATE DATABASE ${POSTGRESQL_DATABASE};" "$psql_base"
+  psql -tc "CREATE DATABASE ${POSTGRESQL_DATABASE};" "$database_url"
 
 bundle exec sequel -m ../db/sequel "$database_url"
