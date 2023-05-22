de37a2fe5e29885a28b6887d5e4f60c30f05beba
Alexandra Dunn <dunn.alex@gmail.com>
2023-05-22 12:16:40 -0700

    superskunk: fix db url handling in migrate script

--- superskunk/scripts/db-migrate-seed.sh
+++ superskunk/scripts/db-migrate-seed.sh
@@ -3,11 +3,12 @@ set -e
 
 db-wait.sh "$POSTGRESQL_HOST:$POSTGRESQL_PORT"
 
-database_url="postgres://${POSTGRESQL_USERNAME}:${POSTGRESQL_PASSWORD}@${POSTGRESQL_HOST}/${POSTGRESQL_DATABASE}"
+psql_base="postgres://${POSTGRESQL_USERNAME}:${POSTGRESQL_PASSWORD}@${POSTGRESQL_HOST}"
+database_url="${psql_base}/${POSTGRESQL_DATABASE}"
 
 echo "Ensuring database: $POSTGRESQL_DATABASE exists"
-psql -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRESQL_DATABASE'" "$database_url" | \
+psql -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRESQL_DATABASE'" "$psql_base" | \
   grep -q 1 || \
-  createdb -e -w "$POSTGRESQL_DATABASE" "$database_url"
+  psql -tc "CREATE DATABASE ${POSTGRESQL_DATABASE};" "$psql_base"
 
 bundle exec sequel -m ../db/sequel "$database_url"
