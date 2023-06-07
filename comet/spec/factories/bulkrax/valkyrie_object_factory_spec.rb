424a80179ce1a88cdc9b9944587a3d7292f1db15
tamsin johnson <tomjohnson@ucsb.edu>
2023-06-07 13:08:53 -0700

    comet: move ValkyrieObjectFactory specs to conventional path
    by convention, `spec` paths should mirror the path to the implementation.

--- comet/spec/factories/bulkrax/valkyrie_object_factory_spec.rb
+++ comet/spec/factories/bulkrax/valkyrie_object_factory_spec.rb
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
 RSpec.describe Bulkrax::ValkyrieObjectFactory, :with_admin_set do
   subject(:object_factory) do
     described_class.new(attributes: attributes,
