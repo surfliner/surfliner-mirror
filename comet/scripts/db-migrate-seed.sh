70c215908c62c0e7595775e7545cc57da5fc4334
tamsin johnson <tomjohnson@ucsb.edu>
2023-06-08 15:32:10 -0700

    comet: wait for the solr collection to be pingable before db:seed
    the seed scripts depend on an active solr connection. it's not enough to just
    wait for host:port availability. the actual collection needs to be ready to
    index documents. {core}/admin/ping is the fastest way to check on this.

--- comet/scripts/db-migrate-seed.sh
+++ comet/scripts/db-migrate-seed.sh
@@ -8,4 +8,10 @@ bundle exec sequel -m db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HO
 
 db-wait.sh "$SOLR_HOST:$SOLR_PORT"
 
+while ! curl --silent "$SOLR_URL/admin/ping" | grep -q 'OK'
+do
+  echo "Waiting for Solr core/collection to be pingable on $SOLR_HOST:$SOLR_PORT"
+  sleep 10
+done
+
 bundle exec rails db:seed
