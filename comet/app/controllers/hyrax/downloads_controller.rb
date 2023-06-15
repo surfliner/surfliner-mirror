e3e1aaf6539bce77da75cbe221700bf22ed78d6e
tamsin johnson <tomjohnson@ucsb.edu>
2023-06-15 14:18:47 -0700

    comet: rewrite `DownloadsController` to use redirects to S3
    use presigned URLs in S3 to support download without streaming content through
    the ~comet application.
    
    related to #3285

--- comet/app/controllers/hyrax/downloads_controller.rb
+++ comet/app/controllers/hyrax/downloads_controller.rb
@@ -4,12 +4,13 @@ module Hyrax
   ##
   # Override Hyrax's default downloads controller with a custom one for comet
   class DownloadsController < ApplicationController
+    EXPIRES_IN = 10.minutes.to_i # S3 presigned URL TTL
+
     before_action :authorize_download!
 
     def show
-      file_set_id = params.require(:id)
-      file_set = Hyrax.query_service.find_by(id: file_set_id)
-      send_file_contents(file_set)
+      file_set = Hyrax.query_service.find_by(id: params.require(:id))
+      redirect_to_presigned(file_set)
     end
 
     private
@@ -21,29 +22,13 @@ module Hyrax
       send_file unauthorized_image, status: :unauthorized
     end
 
-    def send_file_contents(file_set)
-      response.headers["Accept-Ranges"] = "bytes"
-      self.status = 200
-      use = params.fetch(:use, :original_file).to_sym
-      file_metadata = find_file_metadata(file_set: file_set, use: use)
-      file = Hyrax.storage_adapter.find_by(id: file_metadata.file_identifier)
-      prepare_file_headers(metadata: file_metadata, file: file)
-      file.rewind
-      self.response_body = file.read
-    end
-
-    def prepare_file_headers(metadata:, file:, inline: false)
-      inline_display = ActiveRecord::Type::Boolean.new.cast(params.fetch(:inline, "false"))
-      response.headers["Content-Disposition"] = "#{inline_display ? "inline" : "attachment"}; filename=#{metadata.original_filename}"
-      response.headers["Content-Type"] = metadata.mime_type
-      response.headers["Content-Length"] ||= (file.try(:size) || metadata.size.first)
-      # Prevent Rack::ETag from calculating a digest over body
-      response.headers["Last-Modified"] = metadata.updated_at.utc.strftime("%a, %d %b %Y %T GMT")
-      self.content_type = metadata.mime_type
-    end
-
-    def content_options(file_metadata)
-      {disposition: "attachment", type: file_metadata.mime_type, filename: file_metadata.original_filename}
+    def disposition
+      case params["disposition"]
+      when "inline"
+        "inline"
+      else
+        "attachment"
+      end
     end
 
     def find_file_metadata(file_set:, use: :original_file)
@@ -52,6 +37,25 @@ module Hyrax
       results = Hyrax.custom_queries.find_many_file_metadata_by_use(resource: file_set, use: use)
 
       results.first || raise(Hyrax::ObjectNotFoundError)
+    end
+
+    def redirect_to_presigned(file_set)
+      file_metadata = find_file_metadata(file_set: file_set, use: use)
+      id = Hyrax.storage_adapter.send(:shrine_id_for, file_metadata.file_identifier)
+      url = Hyrax.storage_adapter.shrine.url(
+        id,
+        expires_in: EXPIRES_IN,
+        response_content_type: file_metadata.mime_type,
+        response_content_disposition: ContentDisposition.format(disposition: disposition, filename: file_metadata.original_filename)
+      )
+
+      redirect_to(url)
+    end
+
+    ##
+    # @return [Symbol]
+    def use
+      params.fetch(:use, :original_file).to_sym
     end
   end
 end
--- comet/spec/controllers/hyrax/downloads_controller_spec.rb
+++ comet/spec/controllers/hyrax/downloads_controller_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Hyrax::DownloadsController, :integration do
       it "sends the original file" do
         get :show, params: {id: file_set.id}
 
-        expect(response.body).to eq "moomin picture"
+        expect(Net::HTTP.get(URI(response.location))).to eq "moomin picture"
       end
 
       context "and there are multiple file members in the FileSet" do
@@ -59,24 +59,34 @@ RSpec.describe Hyrax::DownloadsController, :integration do
           Hyrax.persister.save(resource: text_file_metadata)
         end
 
-        it "downloads the original by default" do
+        it "redirects to a presigned S3 url" do
           get :show, params: {id: file_set.id}
 
-          file.rewind
-          expect(response.body).to eq "moomin picture"
+          expect(response)
+            .to have_attributes(status: 302, location: include("X-Amz-Signature="))
+        end
+
+        it "downloads the original by default" do
+          get :show, params: {id: file_set.id}
+          expect(Net::HTTP.get(URI(response.location))).to eq "moomin picture"
         end
 
         it "resolves download requests by use" do
           get :show, params: {id: file_set.id, use: "extracted_file"}
-
-          expect(response.body).to eq "description of moomin.jpg"
+          expect(Net::HTTP.get(URI(response.location))).to eq "description of moomin.jpg"
         end
 
-        it "populates headers from selected file" do
+        it "populates content disposition from selected file" do
           get :show, params: {id: file_set.id, use: "extracted_file"}
 
-          expect(response.headers.values)
-            .to include("attachment; filename=moomin.txt")
+          expect(response.location)
+            .to include("response-content-disposition=attachment%3B%20filename%3D%22moomin.txt")
+        end
+
+        it "populates mime_type from selected file" do
+          get :show, params: {id: file_set.id, use: "extracted_file"}
+
+          expect(response.location).to include("application%2Foctet-stream")
         end
       end
     end
