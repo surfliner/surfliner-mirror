c501e1f40cd0167999ce270a87befd152b00c3d6
Alexandra Dunn <dunn.alex@gmail.com>
2023-06-23 01:21:57 +0000

    shoreline: teach importer how to grab shapefiles from s3 via comet

--- shoreline/app/services/importer.rb
+++ shoreline/app/services/importer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'cgi'
+
 ##
 # @see doc/deploy.md
 module Importer
@@ -18,10 +20,7 @@ module Importer
       metadata
     end.except("@context")
 
-    Rails.logger.debug "Parsed metadata #{parsed}"
-
-    file_id = metadata["id"]
-    merged_metadata = if parsed["dct_references_s"].nil?
+    merged_metadata = if shapefile_url.nil?
       parsed
     else
       parsed.merge({dct_references_s: geoserver_references(metadata: metadata)})
@@ -30,15 +29,29 @@ module Importer
     unless shapefile_url.nil?
       Rails.logger.info "Importing shapefile #{shapefile_url}"
 
-      publish_to_geoserver(file_url: shapefile_url, file_id: file_id)
-      merged_metadata = merged_metadata.merge(hash_from_geoserver(id: file_id))
+      # GeoServer claims to support uploading shapefiles by providing a url and
+      # passing `url` as the method (instead of `file` and uploading a binary
+      # stream), but this is not true.  Instead we fetch the file ourselves and
+      # upload the binary data.
+      res_hash = fetch_s3_shapefile(uri: URI.parse(shapefile_url), logger: Rails.logger)
+
+      binary = res_hash[:body]
+      shapefile_name = res_hash[:filename]
+
+      begin # go ahead and keep ingesting metadata if the file ingest fails
+        publish_to_geoserver(file: binary, file_id: shapefile_name)
+        merged_metadata = merged_metadata.merge(hash_from_geoserver(id: shapefile_name))
+      rescue => e
+        Rails.logger.error e.message
+        Rails.logger.error e.backtrace
+      end
     end
+
+    Rails.logger.debug "Parsed metadata #{merged_metadata}"
 
     unless is_metadata_valid?(merged_metadata)
       raise "--- ERROR: metadata failed validation against Aardvark schema: #{metadata}"
     end
-
-    # puts "Finalized metadata: #{merged_metadata}"
 
     publish_to_geoblacklight(metadata: merged_metadata)
   rescue => e
@@ -48,24 +61,17 @@ module Importer
     raise e
   end
 
-  # @param [String] file_url
+  # @param [IO] file
   # @param [String] file_id
-  def self.publish_to_geoserver(file_url:, file_id:)
+  def self.publish_to_geoserver(file:, file_id:)
     conn = Geoserver::Publish::Connection.new(
       "url" => "#{ENV["GEOSERVER_INTERNAL_URL"]}/geoserver/rest",
       "user" => ENV["GEOSERVER_ADMIN_USER"],
       "password" => ENV["GEOSERVER_ADMIN_PASSWORD"]
     )
-
-    # file_id = File.basename(file_path, File.extname(file_path))
     workspace = ENV.fetch("GEOSERVER_WORKSPACE", "public")
 
-    puts "-- Publishing to GeoServer as #{file_id}"
-
-    # GeoServer claims to support uploading shapefiles by providing a url and
-    # passing `url` as the method (instead of `file` and uploading a binary
-    # stream), but this is not true.
-    file = URI.parse(file_url).open.read
+    Rails.logger.info "-- Publishing to GeoServer as #{file_id}"
 
     Geoserver::Publish.create_workspace(workspace_name: workspace, connection: conn)
     Geoserver::Publish::DataStore.new(conn).upload(
@@ -77,14 +83,16 @@ module Importer
   rescue => e
     Rails.logger.error "Error ingesting into GeoServer"
     Rails.logger.error "#{e.message}: #{e.inspect}"
-    Rails.logger.error e.backtrace
+    raise e
   end
 
   def self.publish_to_geoblacklight(metadata:)
     solrdoc = JSON.parse(metadata.to_json)
 
+    Rails.logger.debug "Publishing to Solr: #{solrdoc}"
+
     Blacklight.default_index.connection.add(solrdoc)
-    puts "Committing changes to Solr"
+    Rails.logger.info "Committing changes to Solr"
     Blacklight.default_index.connection.commit
   end
 
@@ -92,6 +100,41 @@ module Importer
     {
       gbl_resourceType_sm: get_layer_type("public:#{id}")
     }
+  end
+
+  # @param [URI] uri
+  # @return [Hash] { body: [IO], filename: [Strign] }
+  def self.fetch_s3_shapefile(uri:, logger:)
+    req = Net::HTTP::Get.new(uri)
+    logger.debug "Querying #{uri} ..."
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") { |http|
+      http.request(req)
+    }
+
+    case res
+    when Net::HTTPSuccess
+      # geoserver will always use the final resolved filename of the shapefile
+      # as its ID; we don't know what this is until we follow the redirect from
+      # e.g. comet/downloads/5 and parse out the parameter data from the full S3
+      # URL
+      #
+      # i hate it too :)
+      filename = /\'\'(.+)\.zip$/.match(CGI::parse(uri.query)['response-content-disposition'].first)[1]
+
+      {
+        body: res.body,
+        filename: filename
+      }
+    when Net::HTTPRedirection
+      logger.debug "Got a 30x response: #{res}"
+      fetch_s3_shapefile(uri: URI(res['location']), logger: logger)
+    else
+      logger.debug "Got a non-success HTTP response:"
+      logger.debug res.inspect
+
+      raise "Failed to fetch #{uri}"
+    end
   end
 
   # Validate the provided metadata against geoblacklight-schema-aardvark
@@ -139,7 +182,9 @@ module Importer
   end
 
   def self.geoserver_references(metadata:)
-    JSON.parse(metadata["dct_references_s"].gsub("http://localhost:8080", ENV["GEOSERVER_URL"])).merge(
+    refs = metadata["dct_references_s"] || "{}"
+
+    JSON.parse(refs.gsub("http://localhost:8080", ENV["GEOSERVER_URL"])).merge(
       transform_reference_uris(REFERENCES)
     ).to_json
   end
