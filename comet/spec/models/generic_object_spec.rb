4ed5049a6af1e056722237a3f071506e00423bfc
kibigo! <kibigo@ucsb.edu>
2023-07-21 17:52:04 -0700

    comet: Add specs for EDTF date processing
    The following desired features were not supported and are marked
    “pending” :—
    
    - Open‐ended ranges (the gem supports the old `1970/open` syntax and not
      the new `1970/` one)
    
    - Sets of date ranges (the gem only supports sets of years and not more
      precise values)

--- comet/spec/models/generic_object_spec.rb
+++ comet/spec/models/generic_object_spec.rb
@@ -37,4 +37,176 @@ RSpec.describe GenericObject do
         .to contain_exactly RDF::Literal("etaoin", datatype: property["data_type"])
     end
   end
+
+  describe "date handling" do
+    it "can save and index simple dates" do
+      work.date_created = "1972-12-31"
+      expect(work.date_created.first.object.iso8601).to eq "1972-12-31"
+      expect(work.date_created.first.object.precision).to eq :day
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "1972-12"
+      expect(work.date_created.first.object.iso8601).to eq "1972-12-01"
+      expect(work.date_created.first.object.precision).to eq :month
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "1972"
+      expect(work.date_created.first.object.iso8601).to eq "1972-01-01"
+      expect(work.date_created.first.object.precision).to eq :year
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index simple intervals" do
+      work.date_created = "1970/1972"
+      expect(work.date_created.first.object.from.iso8601).to eq "1970-01-01"
+      expect(work.date_created.first.object.from.precision).to eq :year
+      expect(work.date_created.first.object.to.iso8601).to eq "1972-01-01"
+      expect(work.date_created.first.object.to.precision).to eq :year
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index uncertain and approximate dates" do
+      work.date_created = "1972-12-31?"
+      expect(work.date_created.first.object.iso8601).to eq "1972-12-31"
+      expect(work.date_created.first.object.uncertain?).to eq true
+      expect(work.date_created.first.object.approximate?).to eq false
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "1972-12-31~"
+      expect(work.date_created.first.object.iso8601).to eq "1972-12-31"
+      expect(work.date_created.first.object.uncertain?).to eq false
+      expect(work.date_created.first.object.approximate?).to eq true
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "1972-12-31%"
+      expect(work.date_created.first.object.iso8601).to eq "1972-12-31"
+      expect(work.date_created.first.object.uncertain?).to eq true
+      expect(work.date_created.first.object.approximate?).to eq true
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index unspecified digits from the right" do
+      work.date_created = "1972-12-XX"
+      expect(work.date_created.first.object.iso8601).to eq "1972-12-01"
+      expect(work.date_created.first.object.precision).to eq :day
+      expect(work.date_created.first.object.unspecified?).to eq true
+      expect(work.date_created.first.object.unspecified?(:day)).to eq true
+      expect(work.date_created.first.object.unspecified?(:month)).to eq false
+      expect(work.date_created.first.object.unspecified?(:year)).to eq false
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "1972-XX-XX"
+      expect(work.date_created.first.object.iso8601).to eq "1972-01-01"
+      expect(work.date_created.first.object.precision).to eq :day
+      expect(work.date_created.first.object.unspecified?).to eq true
+      expect(work.date_created.first.object.unspecified?(:day)).to eq true
+      expect(work.date_created.first.object.unspecified?(:month)).to eq true
+      expect(work.date_created.first.object.unspecified?(:year)).to eq false
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "197X-XX-XX"
+      expect(work.date_created.first.object.iso8601).to eq "1970-01-01"
+      expect(work.date_created.first.object.precision).to eq :day
+      expect(work.date_created.first.object.unspecified?).to eq true
+      expect(work.date_created.first.object.unspecified?(:day)).to eq true
+      expect(work.date_created.first.object.unspecified?(:month)).to eq true
+      expect(work.date_created.first.object.unspecified?(:year)).to eq true
+      expect(work.date_created.first.object.unspecified.year).to eq [nil, nil, false, true]
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "19XX-XX-XX"
+      expect(work.date_created.first.object.iso8601).to eq "1900-01-01"
+      expect(work.date_created.first.object.precision).to eq :day
+      expect(work.date_created.first.object.unspecified?).to eq true
+      expect(work.date_created.first.object.unspecified?(:day)).to eq true
+      expect(work.date_created.first.object.unspecified?(:month)).to eq true
+      expect(work.date_created.first.object.unspecified?(:year)).to eq true
+      expect(work.date_created.first.object.unspecified.year).to eq [nil, nil, true, true]
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "19XX"
+      expect(work.date_created.first.object.iso8601).to eq "1900-01-01"
+      expect(work.date_created.first.object.precision).to eq :year
+      expect(work.date_created.first.object.unspecified?).to eq true
+      expect(work.date_created.first.object.unspecified?(:day)).to eq false
+      expect(work.date_created.first.object.unspecified?(:month)).to eq false
+      expect(work.date_created.first.object.unspecified?(:year)).to eq true
+      expect(work.date_created.first.object.unspecified.year).to eq [nil, nil, true, true]
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    # The rdf-edtf gem doesn’t support this syntax yet.
+    xit "can save and index open‐ended intervals" do
+      work.date_created = "1970-01-01/"
+      expect(work.date_created.first.object.open?).to eq true
+      expect(work.date_created.first.object.cover?(Date.today)).to eq true
+      expect(work.date_created.first.object.from.iso8601).to eq "1970-01-01"
+      expect(work.date_created.first.object.from.precision).to eq :day
+      expect(work.date_created.first.object.to).to eq :open
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index uncertain and approximate intervals" do
+      work.date_created = "1970?/1972~"
+      expect(work.date_created.first.object.from.iso8601).to eq "1970-01-01"
+      expect(work.date_created.first.object.from.uncertain?).to eq true
+      expect(work.date_created.first.object.from.approximate?).to eq false
+      expect(work.date_created.first.object.to.iso8601).to eq "1972-01-01"
+      expect(work.date_created.first.object.to.uncertain?).to eq false
+      expect(work.date_created.first.object.to.approximate?).to eq true
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+      work.date_created = "1970-01-01%/1972-12-31"
+      expect(work.date_created.first.object.from.iso8601).to eq "1970-01-01"
+      expect(work.date_created.first.object.from.uncertain?).to eq true
+      expect(work.date_created.first.object.from.approximate?).to eq true
+      expect(work.date_created.first.object.to.iso8601).to eq "1972-12-31"
+      expect(work.date_created.first.object.to.uncertain?).to eq false
+      expect(work.date_created.first.object.to.approximate?).to eq false
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index mixed precision intervals" do
+      work.date_created = "1970/1972-12-31"
+      expect(work.date_created.first.object.from.iso8601).to eq "1970-01-01"
+      expect(work.date_created.first.object.from.precision).to eq :year
+      expect(work.date_created.first.object.to.iso8601).to eq "1972-12-31"
+      expect(work.date_created.first.object.to.precision).to eq :day
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index negative years" do
+      work.date_created = "-0001"
+      expect(work.date_created.first.object.iso8601).to eq "-0001-01-01"
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    it "can save and index very negative years" do
+      work.date_created = "Y-10000"
+      expect(work.date_created.first.object.iso8601).to eq "-10000-01-01"
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+
+    # The rdf-edtf gem doesn’t support this syntax for days yet, only years.
+    xit "can save and index one of a closed range of dates" do
+      work.date_created = "[1970-01-01..1970-01-11]"
+      expect(work.date_created.first.object.choice?).to eq true
+      expect(work.date_created.first.object.to_a).to eq [Date.new(1970, 1, 1),
+        Date.new(1970, 1, 2), Date.new(1970, 1, 3), Date.new(1970, 1, 4),
+        Date.new(1970, 1, 5), Date.new(1970, 1, 6), Date.new(1970, 1, 7),
+        Date.new(1970, 1, 8), Date.new(1970, 1, 9), Date.new(1970, 1, 10),
+        Date.new(1970, 1, 11)]
+      expect { Hyrax.persister.save(resource: work) }.not_to raise_error
+      expect { Hyrax.index_adapter.save(resource: work) }.not_to raise_error
+    end
+  end
 end
